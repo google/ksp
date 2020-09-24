43e45f97b15263e4f082bad678adf4a41073c586
Ting-Yuan Huang <laszio@google.com>
2020-09-23 17:19:47 -0700

    Generate a default kspVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     kotlin("jvm") version "1.4.0" apply false
+}
+
+if (!extra.has("kspVersion")) {
+    val kotlinBaseVersion: String by project
+    val today = LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
+    extra.set("kspVersion", "$kotlinBaseVersion-dev-experimental-$today")
 }
 
 subprojects {
