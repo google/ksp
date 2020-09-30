42a4827edadee1d2d73e0436949ef8b21830f1ab
Jiaxiang Chen <jiaxiang@google.com>
2020-09-29 23:00:32 -0700

    add createKSTypeReferenceFromKSType API to Resolver

---
+++ compiler-plugin/src/main/kotlin/com/google/devtools/ksp/symbol/impl/synthetic/KSTypeReferenceSyntheticImpl.kt
@@ -0,0 +1,28 @@
+package com.google.devtools.ksp.symbol.impl.synthetic
+
+import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.symbol.impl.KSObjectCache
+
+class KSTypeReferenceSyntheticImpl(val ksType: KSType) : KSTypeReference {
+    companion object : KSObjectCache<KSType, KSTypeReferenceSyntheticImpl>() {
+        fun getCached(ksType: KSType) = KSTypeReferenceSyntheticImpl.cache.getOrPut(ksType) { KSTypeReferenceSyntheticImpl(ksType) }
+    }
+
+    override val annotations: List<KSAnnotation> = emptyList()
+
+    override val element: KSReferenceElement? = null
+
+    override val location: Location = NonExistLocation
+
+    override val modifiers: Set<Modifier> = emptySet()
+
+    override val origin: Origin = Origin.SYNTHETIC
+
+    override fun <D, R> accept(visitor: KSVisitor<D, R>, data: D): R {
+        return visitor.visitTypeReference(this, data)
+    }
+
+    override fun resolve(): KSType {
+        return ksType
+    }
+}
