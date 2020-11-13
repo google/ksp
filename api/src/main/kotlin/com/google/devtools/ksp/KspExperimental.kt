7486fed3e66949da9eb556978d342a4bfae07976
Yigit Boyar <yboyar@google.com>
2020-11-12 18:35:06 -0800

    remove unnecessary comment

--- api/src/main/kotlin/com/google/devtools/ksp/KspExperimental.kt
+++ api/src/main/kotlin/com/google/devtools/ksp/KspExperimental.kt
@@ -3,4 +3,4 @@ package com.google.devtools.ksp
 @RequiresOptIn(message = "This API is experimental." +
     "It may be changed in the future without notice or might be removed.")
 @Retention(AnnotationRetention.BINARY)
-annotation class KspExperimental // Opt-in requirement annotation
+annotation class KspExperimental
