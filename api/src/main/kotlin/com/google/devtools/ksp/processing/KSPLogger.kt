d2dd1c79f8a0d199b32bdf887289fd59e79e4a2d
Jiaxiang Chen <jiaxiang@google.com>
2021-02-02 17:57:05 -0800

    change logger behavior to avoid logger being cleared by compiler in multiple rounds

--- api/src/main/kotlin/com/google/devtools/ksp/processing/KSPLogger.kt
+++ api/src/main/kotlin/com/google/devtools/ksp/processing/KSPLogger.kt
@@ -22,12 +22,6 @@ import com.google.devtools.ksp.symbol.KSNode
 
 interface KSPLogger {
 
-    val errorCount: Int
-    val warningCount: Int
-    val infoCount: Int
-    val loggingCount: Int
-    val exceptionCount: Int
-
     fun logging(message: String, symbol: KSNode? = null)
     fun info(message: String, symbol: KSNode? = null)
     fun warn(message: String, symbol: KSNode? = null)
