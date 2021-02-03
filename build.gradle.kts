4183eb4f53affe2a302d96948eeed53de5b2991a
Ting-Yuan Huang <laszio@google.com>
2021-02-03 15:13:24 -0800

    Upgrade to Kotlin 1.4.30 stable release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 plugins {
-    kotlin("jvm") version "1.4.30-RC" apply false
+    kotlin("jvm") version "1.4.30" apply false
 }
 
 if (!extra.has("kspVersion")) {
