0161050e776573ad5003d7dc8a00e5ec484bc53b
Jiaxiang Chen <jiaxiang@google.com>
2021-02-04 14:58:31 -0800

    introduce onError() for processor error handling

--- compiler-plugin/src/main/kotlin/com/google/devtools/ksp/processing/impl/CodeGeneratorImpl.kt
+++ compiler-plugin/src/main/kotlin/com/google/devtools/ksp/processing/impl/CodeGeneratorImpl.kt
@@ -41,6 +41,7 @@ class CodeGeneratorImpl(
 
     val sourceToOutputs: MutableMap<File, MutableSet<File>> = mutableMapOf()
 
+    // This function will also clear `fileOutputStreamMap` which will change the result of `generatedFile`
     internal fun closeFiles() {
         fileOutputStreamMap.keys.forEach{
             fileOutputStreamMap[it]!!.close()
