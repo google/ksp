06d672b8c1374a7df7e9b206e610cc2fcd776dd0
Jiaxiang Chen <jiaxiang@google.com>
2021-03-18 22:21:41 -0700

    Use IdKey for KSTypeArgumentDescriptorImpl and KSTypeReferenceDescriptorImpl to avoid incorrect caching

--- compiler-plugin/testData/api/nestedClassType.kt
+++ compiler-plugin/testData/api/nestedClassType.kt
@@ -1,17 +1,24 @@
 // TEST PROCESSOR: NestedClassTypeProcessor
 // EXPECTED:
+// foo
+// @TypeAnno1
+// COVARIANT String
 // bar
-// , @TypeAnno
-// CONTRAVARIANT Int,COVARIANT String
+// , @TypeAnno2
+// CONTRAVARIANT Int,INVARIANT String
 // END
 // FILE: a.kt
-annotation class TypeAnno
+annotation class TypeAnno1
+annotation class TypeAnno2
 
 class Outer<T> {
     inner class InnerGeneric<P>
     inner class Inner
 }
 
+class G<T>
+
 class C {
-    val bar: Outer<out @TypeAnno String>.InnerGeneric<in Int>
+    val foo: Outer<out @TypeAnno1 String>.Inner
+    val bar: Outer<@TypeAnno2 String>.InnerGeneric<in Int>
 }
