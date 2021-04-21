3f6f3226cbfe0794f703db9b13cd01663ae67ea9
Jiaxiang Chen <jiaxiang@google.com>
2021-04-20 23:50:50 -0700

    recreate deferred symbols in new round

--- integration-tests/src/test/resources/playground/workload/src/main/java/com/example/AClass.kt
+++ integration-tests/src/test/resources/playground/workload/src/main/java/com/example/AClass.kt
@@ -1,10 +1,17 @@
 package com.example
 
 import com.example.annotation.Builder
-import HELLO
+import hello.HELLO
 
 @Builder
 class AClass(private val a: Int, val b: String, val c: Double, val d: HELLO) {
     val p = "$a, $b, $c"
-    fun foo() = p
+    fun foo() = HELLO()
+    val hello = HELLO()
+    var hello2: HELLO = HELLO()
+        get() { return hello2 }
+        private set
+    class innerClass<T: HELLO>
+
+    val generic = innerClass<HELLO>()
 }
