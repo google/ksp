e177b2c9a087caf4d03af0c03a73ac2edd0c89f1
Zac Sweers <pandanomic@gmail.com>
2021-04-28 00:52:13 -0400

    Make SymbolProcessorProvider a fun interface

--- api/src/main/kotlin/com/google/devtools/ksp/processing/SymbolProcessorProvider.kt
+++ api/src/main/kotlin/com/google/devtools/ksp/processing/SymbolProcessorProvider.kt
@@ -3,7 +3,7 @@ package com.google.devtools.ksp.processing
 /**
  * [SymbolProcessorProvider] is the interface used by plugins to integrate into Kotlin Symbol Processing.
  */
-interface SymbolProcessorProvider {
+fun interface SymbolProcessorProvider {
     /**
     * Called by Kotlin Symbol Processing to create the processor.
     *
