d3fe354da1a79ff21bbda312ba1b74a95c0ca136
Ting-Yuan Huang <laszio@google.com>
2021-06-25 17:02:00 -0700

    Update to kotlin 1.5.30-dev-2862
    1) Added tests-common back.
    2) Apdated tests to reflect the change of `toString` of
       KotlinType for java.
    3) Updated for kotlin-gradle-plugin changes.

--- compiler-plugin/testData/api/javaTypes.kt
+++ compiler-plugin/testData/api/javaTypes.kt
@@ -18,13 +18,13 @@
 // WITH_RUNTIME
 // TEST PROCESSOR: TypeComparisonProcessor
 // EXPECTED:
-// (kotlin.String..kotlin.String?) ?= (kotlin.String..kotlin.String?) : true
-// (kotlin.String..kotlin.String?) ?= String : true
-// (kotlin.String..kotlin.String?) ?= String? : true
-// String ?= (kotlin.String..kotlin.String?) : true
+// (String..String?) ?= (String..String?) : true
+// (String..String?) ?= String : true
+// (String..String?) ?= String? : true
+// String ?= (String..String?) : true
 // String ?= String : true
 // String ?= String? : false
-// String? ?= (kotlin.String..kotlin.String?) : true
+// String? ?= (String..String?) : true
 // String? ?= String : true
 // String? ?= String? : true
 // END
--- compiler-plugin/testData/api/javaTypes2.kt
+++ compiler-plugin/testData/api/javaTypes2.kt
@@ -18,13 +18,13 @@
 // WITH_RUNTIME
 // TEST PROCESSOR: TypeComparisonProcessor
 // EXPECTED:
-// (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) ?= (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) : true
-// (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) ?= Array<C> : true
-// (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) ?= Array<D> : true
-// Array<C> ?= (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) : true
+// (Array<(C..C?)>..Array<out (C..C?)>?) ?= (Array<(C..C?)>..Array<out (C..C?)>?) : true
+// (Array<(C..C?)>..Array<out (C..C?)>?) ?= Array<C> : true
+// (Array<(C..C?)>..Array<out (C..C?)>?) ?= Array<D> : true
+// Array<C> ?= (Array<(C..C?)>..Array<out (C..C?)>?) : true
 // Array<C> ?= Array<C> : true
 // Array<C> ?= Array<D> : false
-// Array<D> ?= (kotlin.Array<(C..C?)>..kotlin.Array<out (C..C?)>?) : false
+// Array<D> ?= (Array<(C..C?)>..Array<out (C..C?)>?) : false
 // Array<D> ?= Array<C> : false
 // Array<D> ?= Array<D> : true
 // END
