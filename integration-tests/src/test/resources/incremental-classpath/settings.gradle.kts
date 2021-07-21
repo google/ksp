a1d0b1282658089004ae0da334d598c02a27cfa3
Ting-Yuan Huang <laszio@google.com>
2021-07-21 00:07:31 -0700

    incremental: tests for classpath changes

---
+++ integration-tests/src/test/resources/incremental-classpath/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    val kspVersion: String by settings
+    val kotlinVersion: String by settings
+    val testRepo: String by settings
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
+        kotlin("jvm") version kotlinVersion
+    }
+    repositories {
+        maven(testRepo)
+        gradlePluginPortal()
+        google()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
+    }
+}
+
+rootProject.name = "incremental-test"
+
+include(":workload")
+include(":validator")
+include(":l1")
+include(":l2")
+include(":l3")
