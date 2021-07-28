51f03c2bbe0e27b8ba1100a2413980aeee7b6bd9
Ting-Yuan Huang <laszio@google.com>
2021-07-27 23:41:07 -0700

    JVM: run processors even without Kotlin sources
    If there's no Java either, Gradle will skip the task. (NO-SOURCE)

---
+++ integration-tests/src/test/resources/java-only/test-processor/src/main/kotlin/TestProcessor.kt
@@ -0,0 +1,31 @@
+import com.google.devtools.ksp.processing.*
+import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.validate
+import java.io.OutputStreamWriter
+
+class TestProcessor(
+    val codeGenerator: CodeGenerator,
+    val logger: KSPLogger
+) : SymbolProcessor {
+    var rounds = 0
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        if (++rounds == 1) {
+            codeGenerator.createNewFile(Dependencies(false), "com.example", "Bar", "kt").use { output ->
+                OutputStreamWriter(output).use { writer ->
+                    writer.write("package com.example\n\n")
+                    writer.write("interface Bar\n")
+                }
+            }
+        }
+
+        return emptyList()
+    }
+}
+
+class TestProcessorProvider : SymbolProcessorProvider {
+    override fun create(
+        environment: SymbolProcessorEnvironment
+    ): SymbolProcessor {
+        return TestProcessor(environment.codeGenerator, environment.logger)
+    }
+}
