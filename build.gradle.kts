96595fe5995f818690f4a112d8f69bfb623030d9
Ting-Yuan Huang <laszio@google.com>
2021-08-11 17:06:31 -0700

    Set Java compatibility to Java 8
    so that it doesn't depend on the JDK used to build KSP.
    
    Kotlin compiler defaults to Java 8.

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,4 +71,9 @@ subprojects {
             }
         }
     }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+        targetCompatibility = JavaVersion.VERSION_1_8.toString()
+    }
 }
