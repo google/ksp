1e04cb5233b314b3a1563e9e45e4f7dd04fb0fac
Ting-Yuan Huang <laszio@google.com>
2021-09-14 17:35:38 -0700

    gradle-plugin: Update overly conservative input annotations
    Classpath implies PathSensitive(PathSensitivity.RELATIVE)
    
    (cherry picked from commit 14d4a0409de5d6e1625dd1227c884a99dc45b77e)

---
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/BuildCacheIT.kt
@@ -0,0 +1,41 @@
+package com.google.devtools.ksp.test
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+
+class BuildCacheIT {
+    @Rule
+    @JvmField
+    val project1: TemporaryTestProject = TemporaryTestProject("buildcache", "playground")
+
+    @Rule
+    @JvmField
+    val project2: TemporaryTestProject = TemporaryTestProject("buildcache", "playground")
+
+    @Test
+    fun testBuildCache() {
+        val buildCacheDir = File(project1.root, "build-cache").absolutePath
+        File(project1.root, "gradle.properties").appendText("\nbuildCacheDir=$buildCacheDir")
+        File(project2.root, "gradle.properties").appendText("\nbuildCacheDir=$buildCacheDir")
+
+        GradleRunner.create().withProjectDir(project1.root).withArguments(
+            "--build-cache",
+            ":workload:clean",
+            "build"
+        ).build().let {
+            Assert.assertEquals(TaskOutcome.SUCCESS, it.task(":workload:kspKotlin")?.outcome)
+        }
+
+        GradleRunner.create().withProjectDir(project2.root).withArguments(
+            "--build-cache",
+            ":workload:clean",
+            "build"
+        ).build().let {
+            Assert.assertEquals(TaskOutcome.FROM_CACHE, it.task(":workload:kspKotlin")?.outcome)
+        }
+    }
+}
