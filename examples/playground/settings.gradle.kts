13bdadeb6d69e213e16fa2027dbd16c0ab8942dd
Ting-Yuan Huang <laszio@google.com>
2021-10-11 22:18:21 -0700

    Import contents of playground.zip into examples/
    (cherry picked from commit 57887f5d0ec72e80b624d89e8d794302c322592d)

---
+++ examples/playground/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kspVersion: String by settings
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
+        kotlin("jvm") version kotlinVersion
+    }
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "playground"
+
+include(":workload")
+include(":test-processor")
