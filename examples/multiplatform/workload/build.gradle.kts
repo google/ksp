034a84eeaacdc845309721a37a8297086223d892
Ting-Yuan Huang <laszio@google.com>
2021-10-11 22:57:22 -0700

    Add an example for multiplatform.
    (cherry picked from commit 9466a5a5edb7008e06c14c7c084e97ff9938d388)

---
+++ examples/multiplatform/workload/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.google.devtools.ksp")
+}
+
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvm {
+        withJava()
+    }
+    js() {
+        browser()
+        nodejs()
+    }
+    linuxX64() {
+        binaries {
+            executable()
+        }
+    }
+    // requires Android SDK
+    androidNativeX64() {
+        binaries {
+            executable()
+        }
+    }
+    // requires Android SDK
+    androidNativeArm64() {
+        binaries {
+            executable()
+        }
+    }
+    mingwX64()
+    sourceSets {
+        val commonMain by getting
+        val linuxX64Main by getting
+        val linuxX64Test by getting
+        val androidNativeX64Main by getting
+        val androidNativeArm64Main by getting
+    }
+}
+
+dependencies {
+    /*
+    add("kspMetadata", project(":test-processor"))
+    add("kspJvm", project(":test-processor"))
+    add("kspJvmTest", project(":test-processor"))
+    add("kspJs", project(":test-processor"))
+    add("kspJsTest", project(":test-processor"))
+    add("kspAndroidNativeX64", project(":test-processor"))
+    add("kspAndroidNativeX64Test", project(":test-processor"))
+    add("kspAndroidNativeArm64", project(":test-processor"))
+    add("kspAndroidNativeArm64Test", project(":test-processor"))
+    add("kspLinuxX64", project(":test-processor"))
+    add("kspLinuxX64Test", project(":test-processor"))
+    add("kspMingwX64", project(":test-processor"))
+    add("kspMingwX64Test", project(":test-processor"))
+    */
+    // FIXME: The universal "ksp" configuration has performance issue and is deprecated on multiplatform.
+    //        Replace it with the above after 1.0.1.
+    ksp(project(":test-processor"))
+}
