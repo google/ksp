e250c3729aea3be78d9276fa6fca4e753e7ad2e9
bennyhuo <bennyhuo@kotliner.cn>
2021-10-23 23:36:23 +0800

    Add support for Java primitives and arrays.
    (cherry picked from commit d1d38bca1bd2f4f74a334f0a5db2adcd79c3344f)

--- compiler-plugin/src/test/java/com/google/devtools/ksp/test/KotlinKSPTestGenerated.java
+++ compiler-plugin/src/test/java/com/google/devtools/ksp/test/KotlinKSPTestGenerated.java
@@ -82,6 +82,11 @@ public class KotlinKSPTestGenerated extends AbstractKotlinKSPTest {
         runTest("testData/api/annotationWithDefault.kt");
     }
 
+    @TestMetadata("annotationWithJavaTypeValue.kt")
+    public void testAnnotationWithJavaTypeValue() throws Exception {
+        runTest("testData/api/annotationWithJavaTypeValue.kt");
+    }
+
     @TestMetadata("asMemberOf.kt")
     public void testAsMemberOf() throws Exception {
         runTest("testData/api/asMemberOf.kt");
