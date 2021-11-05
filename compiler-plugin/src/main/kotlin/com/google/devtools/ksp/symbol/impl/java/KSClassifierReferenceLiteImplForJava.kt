fc1d948ca5bcc04299e2157f92448d14795e8450
Jiaxiang Chen <jiaxiang@google.com>
2021-11-05 01:58:12 -0700

    support qualifier for KSClassifierReferenceLiteImplForJava
    (cherry picked from commit 0e25dd0e468173e9f7ed3b188c02b5c8720dd637)

--- compiler-plugin/src/main/kotlin/com/google/devtools/ksp/symbol/impl/java/KSClassifierReferenceLiteImplForJava.kt
+++ compiler-plugin/src/main/kotlin/com/google/devtools/ksp/symbol/impl/java/KSClassifierReferenceLiteImplForJava.kt
@@ -10,16 +10,27 @@ import com.google.devtools.ksp.symbol.impl.KSObjectCache
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiMethod
 
-class KSClassifierReferenceLiteImplForJava(override val parent: KSTypeReferenceLiteJavaImpl) : KSClassifierReference {
-    companion object : KSObjectCache<KSTypeReferenceLiteJavaImpl, KSClassifierReference>() {
-        fun getCached(parent: KSTypeReferenceLiteJavaImpl) = KSClassifierReferenceLiteImplForJava.cache
-            .getOrPut(parent) { KSClassifierReferenceLiteImplForJava(parent) }
+class KSClassifierReferenceLiteImplForJava(
+    override val parent: KSTypeReferenceLiteJavaImpl,
+    private val name: String?
+) : KSClassifierReference {
+    companion object : KSObjectCache<Pair<KSTypeReferenceLiteJavaImpl, String?>, KSClassifierReference>() {
+        fun getCached(parent: KSTypeReferenceLiteJavaImpl, name: String? = null) =
+            KSClassifierReferenceLiteImplForJava.cache
+                .getOrPut(Pair(parent, name)) { KSClassifierReferenceLiteImplForJava(parent, name) }
     }
-    override val qualifier: KSClassifierReference? = null
+    override val qualifier: KSClassifierReference? by lazy {
+        val referencedName = referencedName()
+        if (referencedName.lastIndexOf('.') == -1) {
+            null
+        } else {
+            getCached(parent, referencedName.substringBeforeLast('.'))
+        }
+    }
 
     override fun referencedName(): String {
-        return when (parent.psiElement) {
-            is PsiAnnotation -> parent.psiElement.nameReferenceElement?.referenceName ?: "<ERROR>"
+        return name ?: when (parent.psiElement) {
+            is PsiAnnotation -> parent.psiElement.nameReferenceElement?.text ?: "<ERROR>"
             is PsiMethod -> parent.psiElement.name
             else -> throw IllegalStateException(
                 "Unexpected psi type in KSTypeReferenceLiteJavaImpl: ${parent.psiElement.javaClass}, $ExceptionMessage"
