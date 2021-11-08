a4a578b4965e125b215c906c829905681975fef5
Jiaxiang Chen <jiaxiang@google.com>
2021-11-07 23:46:52 -0800

    * All KSSymbols skeleton. * use same bootstrap version for compiler and analysis api. * implemented some no resolution involved KSSymbol functions, many are still missing, e.g. modifiers conversion (this should be able to be reused from current frontend implementation). * implemented simple resolution logic as analysis api resolution concept proofing. * Note that analysis API unifies symbols implementation, so there is no need to have 3 implementations for java symbols, kotlin symbols, and descriptor symbols anymore, however, for certain functions, we still need to type check the wrapped element inside a KtSymbol and performance operations accordingly.

---
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSFileSymbolImpl.kt
@@ -0,0 +1,55 @@
+package com.google.devtools.ksp.impl.symbol.kotlin
+
+import com.google.devtools.ksp.symbol.KSAnnotation
+import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSFile
+import com.google.devtools.ksp.symbol.KSName
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.KSVisitor
+import com.google.devtools.ksp.symbol.Location
+import com.google.devtools.ksp.symbol.Origin
+import org.jetbrains.kotlin.analysis.api.analyseWithReadAction
+import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KtNamedClassOrObjectSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KtPropertySymbol
+import org.jetbrains.kotlin.psi.KtFile
+
+class KSFileSymbolImpl(private val ktFile: KtFile) : KSFile {
+    override val packageName: KSName by lazy {
+        KSNameImpl(ktFile.packageFqName.asString())
+    }
+    override val fileName: String by lazy {
+        ktFile.name
+    }
+    override val filePath: String by lazy {
+        ktFile.virtualFilePath
+    }
+    override val declarations: Sequence<KSDeclaration> by lazy {
+        analyseWithReadAction(ktFile) {
+            ktFile.getFileSymbol().getFileScope().getAllSymbols().map {
+                when (it) {
+                    is KtNamedClassOrObjectSymbol -> KSClassDeclarationImpl(it)
+                    is KtFunctionSymbol -> KSFunctionDeclarationImpl(it)
+                    is KtPropertySymbol -> KSPropertyDeclarationImpl(it)
+                    else -> throw IllegalStateException("Unhandled ")
+                }
+            }
+        }
+    }
+    override val origin: Origin = Origin.KOTLIN
+
+    override val location: Location
+        get() = TODO("Not yet implemented")
+
+    override val parent: KSNode? = null
+
+    override fun <D, R> accept(visitor: KSVisitor<D, R>, data: D): R {
+        return visitor.visitFile(this, data)
+    }
+
+    override val annotations: Sequence<KSAnnotation> by lazy {
+        analyseWithReadAction(ktFile) {
+            ktFile.getFileSymbol().annotations.map { KSAnnotationImpl(it) }.asSequence()
+        }
+    }
+}
