3d54ef7acc7f35e47a9019cf6e79cf6c80fe4938
Ting-Yuan Huang <laszio@google.com>
2021-12-07 11:27:12 -0800

    Update to Kotlin 1.6.10-RC
    (cherry picked from commit 926c2e65c20c1ab6f235e29d2fec07b8541cc172)
    (cherry picked from commit f345fc098ecce3570be7959330d6cfd9279fd391)

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
@@ -577,6 +577,7 @@ abstract class KspTaskJvm : KotlinCompile(KotlinJvmOptionsImpl()), KspTask {
         args: K2JVMCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
+        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (isKspIncremental) {
             if (isIntermoduleIncremental) {
@@ -594,7 +595,7 @@ abstract class KspTaskJvm : KotlinCompile(KotlinJvmOptionsImpl()), KspTask {
             clearIncCache()
         }
         args.addChangedFiles(changedFiles)
-        super.callCompilerAsync(args, sourceRoots, changedFiles)
+        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
     }
 
     override fun skipCondition(): Boolean = false
@@ -669,13 +670,14 @@ abstract class KspTaskJS @Inject constructor(
         args: K2JSCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
+        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (!isKspIncremental || changedFiles.hasNonSourceChange()) {
             clearIncCache()
         } else {
             args.addChangedFiles(changedFiles)
         }
-        super.callCompilerAsync(args, sourceRoots, changedFiles)
+        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
     }
 
     // Overrding an internal function is hacky.
@@ -744,13 +746,14 @@ abstract class KspTaskMetadata : KotlinCompileCommon(KotlinMultiplatformCommonOp
         args: K2MetadataCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
+        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (!isKspIncremental || changedFiles.hasNonSourceChange()) {
             clearIncCache()
         } else {
             args.addChangedFiles(changedFiles)
         }
-        super.callCompilerAsync(args, sourceRoots, changedFiles)
+        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
     }
 }
 
