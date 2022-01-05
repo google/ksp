a16dae0018dc484e76c819b25cfd6d996af89d38
Jiaxiang Chen <jiaxiang@google.com>
2022-01-05 13:12:40 -0800

    add KSP benchmark * add an exhaustive processor that visits all files and invokes APIs * apply a patch to Tachiyomi for applying exhaustive processor.
    (cherry picked from commit 60b69caa7fd0757046e8cf87b538863cc6ca8576)

---
+++ benchmark/exhaustive-processor/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kspVersion: String by settings
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
+        kotlin("jvm") version kotlinVersion
+    }
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "exhaustive-processor"
+
+include(":processor")
