06f275871e9d2f4f5e23731272efac43bbebc7c5
Jiaxiang Chen <jiaxiang@google.com>
2022-01-12 02:55:08 -0800

    change configure logic for new MPP common code task logic
    (cherry picked from commit 42ef4e0b1696ddbcabbb188d7761c8692ca9f988)

--- integration-tests/src/test/resources/kmp/workload-linuxX64/build.gradle.kts
+++ integration-tests/src/test/resources/kmp/workload-linuxX64/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
 }
 
 dependencies {
-    add("kspMetadata", project(":test-processor"))
+    add("kspCommonMainMetadata", project(":test-processor"))
     add("kspJvm", project(":test-processor"))
     add("kspJvmTest", project(":test-processor"))
     add("kspLinuxX64", project(":test-processor"))
