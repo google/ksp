724a9e03f6d7f2eca963a99097701bf214752862
Jinseong Jeon <jsjeon@google.com>
2022-01-25 15:20:56 -0800

    Unregister unrelated components

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
@@ -1,28 +1,13 @@
 package com.google.devtools.ksp.impl
 
-import com.intellij.core.CoreApplicationEnvironment
-import com.intellij.formatting.Formatter
-import com.intellij.formatting.FormatterImpl
 import com.intellij.mock.MockApplication
 import com.intellij.mock.MockProject
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.AsyncExecutionService
-import com.intellij.openapi.application.impl.AsyncExecutionServiceImpl
-import com.intellij.openapi.editor.impl.DocumentWriteAccessGuard
-import com.intellij.openapi.extensions.Extensions
-import com.intellij.openapi.fileEditor.impl.FileEditorPsiTreeChangeListener
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.openapi.vfs.CharsetToolkit
-import com.intellij.pom.PomModel
-import com.intellij.pom.tree.TreeAspect
-import com.intellij.psi.PsiTreeChangeListener
-import com.intellij.psi.codeStyle.CodeStyleManager
-import com.intellij.psi.impl.DocumentCommitProcessor
-import com.intellij.psi.impl.DocumentCommitThread
 import com.intellij.psi.impl.PsiFileFactoryImpl
-import com.intellij.psi.impl.source.codeStyle.CodeStyleManagerImpl
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.testFramework.LightVirtualFile
 import org.jetbrains.kotlin.analysis.api.InvalidWayOfUsingAnalysisSession
@@ -117,40 +102,15 @@ fun registerComponents(project: MockProject, environment: KotlinCoreEnvironment,
         application.registerService(
             KotlinReferenceProviderContributor::class.java, KotlinFirReferenceContributor::class.java
         )
-        application.registerService(DocumentCommitProcessor::class.java, DocumentCommitThread::class.java)
-        application.registerService(AsyncExecutionService::class.java, AsyncExecutionServiceImpl::class.java)
-        application.registerService(Formatter::class.java, FormatterImpl::class.java)
     }
-
-    project.picoContainer.registerComponentInstance(
-        CodeStyleManager::class.qualifiedName,
-        CodeStyleManagerImpl(project)
-    )
-    project.picoContainer.registerComponentInstance(
-        TreeAspect::class.qualifiedName,
-        TreeAspect()
-    )
-    project.picoContainer.registerComponentInstance(
-        PomModel::class.qualifiedName,
-        HackPomModel(project)
-    )
 
     project.picoContainer.registerComponentInstance(
         KtModuleScopeProvider::class.qualifiedName,
         KtModuleScopeProviderImpl()
     )
 
-    CoreApplicationEnvironment.registerExtensionPoint(
-        Extensions.getRootArea(),
-        DocumentWriteAccessGuard.EP_NAME,
-        DocumentWriteAccessGuard::class.java
-    )
-    CoreApplicationEnvironment.registerExtensionPoint(
-        project.extensionArea,
-        PsiTreeChangeListener.EP,
-        FileEditorPsiTreeChangeListener::class.java
-    )
 }
+
 fun findSomeReference(ktFile: KtFile): KtReference? {
     for (i in 1..300) {
         val reference = ktFile.findReferenceAt(i)
