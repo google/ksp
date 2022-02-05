cd1163ac49a7ebd0ddbfd1d5a8d4dadf26b38a1e
Jiaxiang Chen <jiaxiang@google.com>
2022-02-04 22:37:30 -0800

    reorganize sources and java doc jar publish task
    (cherry picked from commit e1f0a23521b3ef9aa6f4cbc83f8d1f43268e698b)

--- symbol-processing/build.gradle.kts
+++ symbol-processing/build.gradle.kts
@@ -1,5 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
+evaluationDependsOn(":common-util")
+evaluationDependsOn(":compiler-plugin")
+
 val kotlinBaseVersion: String by project
 val signingKey: String? by project
 val signingPassword: String? by project
@@ -30,12 +33,7 @@ tasks {
     publish {
         dependsOn(shadowJar)
         dependsOn(project(":compiler-plugin").tasks["dokkaJavadocJar"])
-    }
-
-    val sourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        from(project(":compiler-plugin").sourceSets.main.get().allSource)
-        from(project(":common-util").sourceSets.main.get().allSource)
+        dependsOn(project(":compiler-plugin").tasks["sourcesJar"])
     }
 }
 
@@ -43,8 +41,8 @@ publishing {
     publications {
         create<MavenPublication>("shadow") {
             artifactId = "symbol-processing"
-            artifact(tasks["sourcesJar"])
             artifact(project(":compiler-plugin").tasks["dokkaJavadocJar"])
+            artifact(project(":compiler-plugin").tasks["sourcesJar"])
             artifact(tasks["shadowJar"])
             pom {
                 name.set("com.google.devtools.ksp:symbol-processing")
