51fcfb8de383a1e2469f6786dc6fdbec0baddaa3
Jiaxiang Chen <jiaxiang@google.com>
2022-02-08 23:50:09 -0800

    use index to track memoized sequence iterator status

--- common-util/src/main/kotlin/com/google/devtools/ksp/MemoizedSequence.kt
+++ common-util/src/main/kotlin/com/google/devtools/ksp/MemoizedSequence.kt
@@ -3,29 +3,29 @@ package com.google.devtools.ksp
 // TODO: garbage collect underlying sequence after exhaust.
 class MemoizedSequence<T>(sequence: Sequence<T>) : Sequence<T> {
 
-    private val cache = mutableListOf<T>()
+    private val cache = arrayListOf<T>()
 
     private val iter: Iterator<T> by lazy {
         sequence.iterator()
     }
 
-    private inner class CachedIterator(val iterCache: Iterator<T>, var buffer: Int) : Iterator<T> {
+    private inner class CachedIterator() : Iterator<T> {
+        var idx = 0
         override fun hasNext(): Boolean {
-            return buffer > 0 || iter.hasNext()
+            return idx < cache.size || iter.hasNext()
         }
 
         override fun next(): T {
-            if (buffer > 0) {
-                buffer --
-                return iterCache.next()
+            if (idx == cache.size) {
+                cache.add(iter.next())
             }
-            val value = iter.next()
-            cache.add(value)
+            val value = cache[idx]
+            idx += 1
             return value
         }
     }
 
     override fun iterator(): Iterator<T> {
-        return CachedIterator(cache.iterator(), cache.size)
+        return CachedIterator()
     }
 }
