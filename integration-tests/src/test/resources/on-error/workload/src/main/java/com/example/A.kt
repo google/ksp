d51046ab54d6efa59a097a944e505fa052159df6
Ting-Yuan Huang <laszio@google.com>
2022-02-14 21:45:34 -0800

    Return error to compiler via message collector
    instead of AnalysisResult.compilationError or
    AnalysisResult.internalError, which are buggy and not maintained.

--- integration-tests/src/test/resources/on-error/workload/src/main/java/com/example/A.kt
+++ integration-tests/src/test/resources/on-error/workload/src/main/java/com/example/A.kt
@@ -1,5 +1,11 @@
 package com.example
 
+@RequiresOptIn
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class MyOptIn
+
+@OptIn(MyOptIn::class)
 fun main() {
     print("hello world")
 }
