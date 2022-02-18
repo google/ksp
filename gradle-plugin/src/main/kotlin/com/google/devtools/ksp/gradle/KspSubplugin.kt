85440b504823fc3351b64a901467c405f6d9c79a
Jiaxiang Chen <jiaxiang@google.com>
2022-02-17 22:10:06 -0800

    Revert "Update to Kotlin 1.6.10-RC"
    This reverts commit 3d54ef7acc7f35e47a9019cf6e79cf6c80fe4938.

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
@@ -582,7 +582,6 @@ abstract class KspTaskJvm : KotlinCompile(KotlinJvmOptionsImpl()), KspTask {
         args: K2JVMCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
-        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (isKspIncremental) {
             if (isIntermoduleIncremental) {
@@ -600,7 +599,7 @@ abstract class KspTaskJvm : KotlinCompile(KotlinJvmOptionsImpl()), KspTask {
             clearIncCache()
         }
         args.addChangedFiles(changedFiles)
-        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
+        super.callCompilerAsync(args, sourceRoots, changedFiles)
     }
 
     override fun skipCondition(): Boolean = false
@@ -675,14 +674,13 @@ abstract class KspTaskJS @Inject constructor(
         args: K2JSCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
-        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (!isKspIncremental || changedFiles.hasNonSourceChange()) {
             clearIncCache()
         } else {
             args.addChangedFiles(changedFiles)
         }
-        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
+        super.callCompilerAsync(args, sourceRoots, changedFiles)
     }
 
     // Overrding an internal function is hacky.
@@ -751,14 +749,13 @@ abstract class KspTaskMetadata : KotlinCompileCommon(KotlinMultiplatformCommonOp
         args: K2MetadataCompilerArguments,
         sourceRoots: SourceRoots,
         changedFiles: ChangedFiles,
-        taskOutputsBackup: TaskOutputsBackup?,
     ) {
         if (!isKspIncremental || changedFiles.hasNonSourceChange()) {
             clearIncCache()
         } else {
             args.addChangedFiles(changedFiles)
         }
-        super.callCompilerAsync(args, sourceRoots, changedFiles, taskOutputsBackup)
+        super.callCompilerAsync(args, sourceRoots, changedFiles)
     }
 }
 
