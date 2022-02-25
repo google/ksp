c0ecb97a8313dfc8b6f08f0e3db67ffc49dab34e
Jinseong Jeon <jsjeon@google.com>
2022-02-24 23:33:16 -0800

    Use Analysis API standalone mode entry points

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
@@ -3,99 +3,24 @@ package com.google.devtools.ksp.impl
 import com.intellij.mock.MockApplication
 import com.intellij.mock.MockProject
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.util.text.StringUtilRt
-import com.intellij.openapi.vfs.CharsetToolkit
-import com.intellij.psi.impl.PsiFileFactoryImpl
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.testFramework.LightVirtualFile
-import org.jetbrains.kotlin.analysis.api.InvalidWayOfUsingAnalysisSession
-import org.jetbrains.kotlin.analysis.api.KtAnalysisSessionProvider
+import com.intellij.openapi.vfs.StandardFileSystems
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
+import com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.analysis.api.standalone.configureApplicationEnvironment
+import org.jetbrains.kotlin.analysis.api.standalone.configureProjectEnvironment
 import org.jetbrains.kotlin.analysis.api.analyseWithReadAction
-import org.jetbrains.kotlin.analysis.api.fir.KtFirAnalysisSessionProvider
-import org.jetbrains.kotlin.analysis.api.impl.base.references.HLApiReferenceProviderService
 import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.services.FirSealedClassInheritorsProcessorFactory
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.services.PackagePartProviderFactory
-import org.jetbrains.kotlin.analysis.project.structure.KtModuleScopeProvider
-import org.jetbrains.kotlin.analysis.project.structure.KtModuleScopeProviderImpl
-import org.jetbrains.kotlin.analysis.project.structure.ProjectStructureProvider
-import org.jetbrains.kotlin.analysis.providers.impl.KotlinStaticDeclarationProviderFactory
-import org.jetbrains.kotlin.analysis.providers.impl.KotlinStaticModificationTrackerFactory
-import org.jetbrains.kotlin.analysis.providers.impl.KotlinStaticPackageProviderFactory
-import org.jetbrains.kotlin.analysis.providers.*
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.idea.references.*
-import org.jetbrains.kotlin.load.kotlin.PackagePartProvider
-import org.jetbrains.kotlin.psi.KotlinReferenceProvidersService
 import org.jetbrains.kotlin.psi.KtFile
 import java.io.File
-
-@OptIn(InvalidWayOfUsingAnalysisSession::class)
-fun registerComponents(project: MockProject, environment: KotlinCoreEnvironment, ktFiles: List<KtFile>) {
-
-    project.picoContainer.registerComponentInstance(
-        KtAnalysisSessionProvider::class.qualifiedName,
-        KtFirAnalysisSessionProvider(project)
-    )
-
-    project.picoContainer.registerComponentInstance(
-        ProjectStructureProvider::class.qualifiedName,
-        ProjectStructureProviderImpl()
-    )
-
-    project.picoContainer.registerComponentInstance(
-        KotlinModificationTrackerFactory::class.qualifiedName,
-        KotlinStaticModificationTrackerFactory()
-    )
-
-    RegisterComponentService.registerFirIdeResolveStateService(project)
-
-    project.picoContainer.registerComponentInstance(
-        KotlinDeclarationProviderFactory::class.qualifiedName,
-        KotlinStaticDeclarationProviderFactory(ktFiles)
-    )
-
-    project.picoContainer.registerComponentInstance(
-        KotlinPackageProviderFactory::class.qualifiedName,
-        KotlinStaticPackageProviderFactory(ktFiles)
-    )
-
-    project.picoContainer.registerComponentInstance(
-        FirSealedClassInheritorsProcessorFactory::class.qualifiedName,
-        FirSealedClassInheritorsProcessorFactoryImpl()
-    )
-    project.picoContainer.registerComponentInstance(
-        PackagePartProviderFactory::class.qualifiedName,
-        object : PackagePartProviderFactory() {
-            override fun createPackagePartProviderForLibrary(scope: GlobalSearchScope): PackagePartProvider {
-                return environment.createPackagePartProvider(scope)
-            }
-        }
-    )
-
-    val application = ApplicationManager.getApplication() as MockApplication
-    KotlinCoreEnvironment.underApplicationLock {
-        application.registerService(
-            KotlinReferenceProvidersService::class.java, HLApiReferenceProviderService::class.java
-        )
-        application.registerService(
-            KotlinReferenceProviderContributor::class.java, KotlinFirReferenceContributor::class.java
-        )
-    }
-
-    project.picoContainer.registerComponentInstance(
-        KtModuleScopeProvider::class.qualifiedName,
-        KtModuleScopeProviderImpl()
-    )
-
-}
+import org.jetbrains.kotlin.cli.jvm.config.addJavaSourceRoot
 
 fun findSomeReference(ktFile: KtFile): KtReference? {
     for (i in 1..300) {
@@ -107,6 +32,18 @@ fun findSomeReference(ktFile: KtFile): KtReference? {
     return null
 }
 
+private fun convertFilesToKtFiles(project: Project, files: List<File>): List<KtFile> {
+    val fs = StandardFileSystems.local()
+    val psiManager = PsiManager.getInstance(project)
+    val ktFiles = mutableListOf<KtFile>()
+    for (file in files) {
+        val vFile = fs.findFileByPath(file.absolutePath) ?: continue
+        val ktFile = psiManager.findFile(vFile) as? KtFile ?: continue
+        ktFiles.add(ktFile)
+    }
+    return ktFiles
+}
+
 fun main() {
     val compilerConfiguration = CompilerConfiguration()
     compilerConfiguration.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
@@ -115,18 +52,27 @@ fun main() {
         EnvironmentConfigFiles.JVM_CONFIG_FILES
     )
 
+    val application = ApplicationManager.getApplication() as MockApplication
+    configureApplicationEnvironment(application)
+
+    val file = File("testData/api/hello.kt").absoluteFile
+    compilerConfiguration.addJavaSourceRoot(file)
+
     val project = env.project as MockProject
-    val factory = PsiFileFactoryImpl(project)
+    val ktFiles = convertFilesToKtFiles(project, listOf(file))
+    configureProjectEnvironment(
+        project,
+        compilerConfiguration,
+        ktFiles,
+        env::createPackagePartProvider,
+        env.projectEnvironment.environment.jarFileSystem as CoreJarFileSystem
+    )
 
-    val file = File("testData/api/hello.kt")
-    val text = FileUtil.loadFile(file, CharsetToolkit.UTF8, true).trim { it <= ' ' }
-
-    val virtualFile = LightVirtualFile("a.kt", KotlinLanguage.INSTANCE, StringUtilRt.convertLineSeparators(text))
-    val ktFile = factory.trySetupPsiForFile(virtualFile, KotlinLanguage.INSTANCE, true, false) as KtFile
-    registerComponents(project, env, listOf(ktFile))
-    analyseWithReadAction(ktFile) {
-        val fileSymbol = ktFile.getFileSymbol()
-        val members = fileSymbol.getFileScope().getAllSymbols()
-        members.filterIsInstance<KtFunctionSymbol>()
+    for (ktFile in ktFiles) {
+        analyseWithReadAction(ktFile) {
+            val fileSymbol = ktFile.getFileSymbol()
+            val members = fileSymbol.getFileScope().getAllSymbols()
+            members.filterIsInstance<KtFunctionSymbol>()
+        }
     }
 }
