9d5451d746fd3e4cfec19b655a3ed74fe4e475a7
Jiaxiang Chen <jiaxiang@google.com>
2022-03-04 23:29:51 -0800

    implement PSI based API logics for kotlin source, with the exception of the following:
    * necesssary API not available in analysis API as of 1.7.0-dev-1984
    * type ralated logics
    * parent look up

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSValueParameterImpl.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSValueParameterImpl.kt
@@ -1,19 +1,14 @@
 package com.google.devtools.ksp.impl.symbol.kotlin
 
-import com.google.devtools.ksp.symbol.KSAnnotation
-import com.google.devtools.ksp.symbol.KSName
-import com.google.devtools.ksp.symbol.KSNode
-import com.google.devtools.ksp.symbol.KSTypeReference
-import com.google.devtools.ksp.symbol.KSValueParameter
-import com.google.devtools.ksp.symbol.KSVisitor
-import com.google.devtools.ksp.symbol.Location
-import com.google.devtools.ksp.symbol.Origin
+import com.google.devtools.ksp.symbol.*
 import org.jetbrains.kotlin.analysis.api.annotations.annotations
 import org.jetbrains.kotlin.analysis.api.symbols.KtValueParameterSymbol
 
 class KSValueParameterImpl(private val ktValueParameterSymbol: KtValueParameterSymbol) : KSValueParameter {
-    override val name: KSName?
-        get() = TODO("Not yet implemented")
+    override val name: KSName? by lazy {
+        KSNameImpl(ktValueParameterSymbol.name.asString())
+    }
+
     override val type: KSTypeReference by lazy {
         KSTypeReferenceImpl(ktValueParameterSymbol.returnType)
     }
@@ -35,10 +30,13 @@ class KSValueParameterImpl(private val ktValueParameterSymbol: KtValueParameterS
     override val annotations: Sequence<KSAnnotation> by lazy {
         ktValueParameterSymbol.annotations.asSequence().map { KSAnnotationImpl(it) }
     }
-    override val origin: Origin
-        get() = TODO("Not yet implemented")
-    override val location: Location
-        get() = TODO("Not yet implemented")
+    override val origin: Origin by lazy {
+        mapAAOrigin(ktValueParameterSymbol.origin)
+    }
+
+    override val location: Location by lazy {
+        ktValueParameterSymbol.psi?.toLocation() ?: NonExistLocation
+    }
     override val parent: KSNode?
         get() = TODO("Not yet implemented")
 
