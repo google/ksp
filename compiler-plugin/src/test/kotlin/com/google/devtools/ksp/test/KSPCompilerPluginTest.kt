05617c23b8698fc0e199c6b7ea32e11f84bd7e61
Ting-Yuan Huang <laszio@google.com>
2022-03-16 18:55:52 -0700

    Migrate compiler-plugin tests to new framework
    Also updated auto-merger to detect compiler bumps by grepping
    KOTLIN(underline)VERSION(underline)UPDATE

---
+++ compiler-plugin/src/test/kotlin/com/google/devtools/ksp/test/KSPCompilerPluginTest.kt
@@ -0,0 +1,463 @@
+/*
+ * Copyright 2022 Google LLC
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.ksp.test
+
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+
+@Execution(ExecutionMode.SAME_THREAD)
+class KSPCompilerPluginTest : AbstractKSPCompilerPluginTest() {
+    @TestMetadata("annotatedUtil.kt")
+    @Test
+    fun testAnnotatedUtil() {
+        runTest("testData/api/annotatedUtil.kt")
+    }
+
+    @TestMetadata("javaAnnotatedUtil.kt")
+    @Test
+    fun testJavaAnnotatedUtil() {
+        runTest("testData/api/javaAnnotatedUtil.kt")
+    }
+
+    @Test
+    fun testAbstractFunctions() {
+        runTest("testData/api/abstractFunctions.kt")
+    }
+
+    @TestMetadata("allFunctions.kt")
+    @Test
+    fun testAllFunctions() {
+        runTest("testData/api/allFunctions.kt")
+    }
+
+    @TestMetadata("annotationInDependencies.kt")
+    @Test
+    fun testAnnotationsInDependencies() {
+        runTest("testData/api/annotationInDependencies.kt")
+    }
+
+    @TestMetadata("annotationOnConstructorParameter.kt")
+    @Test
+    fun testAnnotationOnConstructorParameter() {
+        runTest("testData/api/annotationOnConstructorParameter.kt")
+    }
+
+    @TestMetadata("annotationWithArbitraryClassValue.kt")
+    @Test
+    fun testAnnotationWithArbitraryClassValue() {
+        runTest("testData/api/annotationWithArbitraryClassValue.kt")
+    }
+
+    @TestMetadata("annotationValue.kt")
+    @Test
+    fun testAnnotationValue() {
+        runTest("testData/api/annotationValue.kt")
+    }
+
+    @TestMetadata("annotationWithArrayValue.kt")
+    @Test
+    fun testAnnotationWithArrayValue() {
+        runTest("testData/api/annotationWithArrayValue.kt")
+    }
+
+    @TestMetadata("annotationWithDefault.kt")
+    @Test
+    fun testAnnotationWithDefault() {
+        runTest("testData/api/annotationWithDefault.kt")
+    }
+
+    @TestMetadata("annotationWithJavaTypeValue.kt")
+    @Test
+    fun testAnnotationWithJavaTypeValue() {
+        runTest("testData/api/annotationWithJavaTypeValue.kt")
+    }
+
+    @TestMetadata("asMemberOf.kt")
+    @Test
+    fun testAsMemberOf() {
+        runTest("testData/api/asMemberOf.kt")
+    }
+
+    @TestMetadata("backingFields.kt")
+    @Test
+    fun testBackingFields() {
+        runTest("testData/api/backingFields.kt")
+    }
+
+    @TestMetadata("builtInTypes.kt")
+    @Test
+    fun testBuiltInTypes() {
+        runTest("testData/api/builtInTypes.kt")
+    }
+
+    @TestMetadata("checkOverride.kt")
+    @Test
+    fun testCheckOverride() {
+        runTest("testData/api/checkOverride.kt")
+    }
+
+    @TestMetadata("classKinds.kt")
+    @Test
+    fun testClassKinds() {
+        runTest("testData/api/classKinds.kt")
+    }
+
+    @TestMetadata("companion.kt")
+    @Test
+    fun testCompanion() {
+        runTest("testData/api/companion.kt")
+    }
+
+    @TestMetadata("constProperties.kt")
+    @Test
+    fun testConstProperties() {
+        runTest("testData/api/constProperties.kt")
+    }
+
+    @TestMetadata("constructorDeclarations.kt")
+    @Test
+    fun testConstructorDeclarations() {
+        runTest("testData/api/constructorDeclarations.kt")
+    }
+
+    @TestMetadata("crossModuleTypeAlias.kt")
+    @Test
+    fun testCrossModuleTypeAlias() {
+        runTest("testData/api/crossModuleTypeAlias.kt")
+    }
+
+    @TestMetadata("declarationInconsistency.kt")
+    @Test
+    fun testDeclarationInconsistency() {
+        runTest("testData/api/declarationInconsistency.kt")
+    }
+
+    @TestMetadata("declarationPackageName.kt")
+    @Test
+    fun testDeclarationPackageName() {
+        runTest("testData/api/declarationPackageName.kt")
+    }
+
+    @TestMetadata("declarationOrder.kt")
+    @Test
+    fun testDeclarationOrder() {
+        runTest("testData/api/declarationOrder.kt")
+    }
+
+    @TestMetadata("declarationUtil.kt")
+    @Test
+    fun testDeclarationUtil() {
+        runTest("testData/api/declarationUtil.kt")
+    }
+
+    @TestMetadata("declared.kt")
+    @Test
+    fun testDeclared() {
+        runTest("testData/api/declared.kt")
+    }
+
+    @TestMetadata("docString.kt")
+    @Test
+    fun testDocString() {
+        runTest("testData/api/docString.kt")
+    }
+
+    @TestMetadata("equivalentJavaWildcards.kt")
+    @Test
+    fun testEquivalentJavaWildcards() {
+        runTest("testData/api/equivalentJavaWildcards.kt")
+    }
+
+    @TestMetadata("errorTypes.kt")
+    @Test
+    fun testErrorTypes() {
+        runTest("testData/api/errorTypes.kt")
+    }
+
+    @TestMetadata("functionTypeAlias.kt")
+    @Test
+    fun testFunctionTypeAlias() {
+        runTest("testData/api/functionTypeAlias.kt")
+    }
+
+    @TestMetadata("functionTypes.kt")
+    @Test
+    fun testFunctionTypes() {
+        runTest("testData/api/functionTypes.kt")
+    }
+
+    @TestMetadata("getPackage.kt")
+    @Test
+    fun testGetPackage() {
+        runTest("testData/api/getPackage.kt")
+    }
+
+    @TestMetadata("getByName.kt")
+    @Test
+    fun testGetByName() {
+        runTest("testData/api/getByName.kt")
+    }
+
+    @TestMetadata("getSymbolsFromAnnotation.kt")
+    @Test
+    fun testGetSymbolsFromAnnotation() {
+        runTest("testData/api/getSymbolsFromAnnotation.kt")
+    }
+
+    @TestMetadata("hello.kt")
+    @Test
+    fun testHello() {
+        runTest("testData/api/hello.kt")
+    }
+
+    @TestMetadata("implicitElements.kt")
+    @Test
+    fun testImplicitElements() {
+        runTest("testData/api/implicitElements.kt")
+    }
+
+    @TestMetadata("implicitPropertyAccessors.kt")
+    @Test
+    fun testImplicitPropertyAccessors() {
+        runTest("testData/api/implicitPropertyAccessors.kt")
+    }
+
+    @TestMetadata("innerTypes.kt")
+    @Test
+    fun testInnerTypes() {
+        runTest("testData/api/innerTypes.kt")
+    }
+
+    @TestMetadata("interfaceWithDefault.kt")
+    @Test
+    fun testInterfaceWithDefault() {
+        runTest("testData/api/interfaceWithDefault.kt")
+    }
+
+    @TestMetadata("javaModifiers.kt")
+    @Test
+    fun testJavaModifiers() {
+        runTest("testData/api/javaModifiers.kt")
+    }
+
+    @TestMetadata("javaToKotlinMapper.kt")
+    @Test
+    fun testJavaToKotlinMapper() {
+        runTest("testData/api/javaToKotlinMapper.kt")
+    }
+
+    @TestMetadata("javaTypes.kt")
+    @Test
+    fun testJavaTypes() {
+        runTest("testData/api/javaTypes.kt")
+    }
+
+    @TestMetadata("javaTypes2.kt")
+    @Test
+    fun testJavaTypes2() {
+        runTest("testData/api/javaTypes2.kt")
+    }
+
+    @TestMetadata("javaWildcards2.kt")
+    @Test
+    fun testJavaWildcards2() {
+        runTest("testData/api/javaWildcards2.kt")
+    }
+
+    @TestMetadata("libOrigins.kt")
+    @Test
+    fun testLibOrigins() {
+        runTest("testData/api/libOrigins.kt")
+    }
+
+    @TestMetadata("makeNullable.kt")
+    @Test
+    fun testMakeNullable() {
+        runTest("testData/api/makeNullable.kt")
+    }
+
+    @TestMetadata("mangledNames.kt")
+    @Test
+    fun testMangledNames() {
+        runTest("testData/api/mangledNames.kt")
+    }
+
+    @TestMetadata("multipleModules.kt")
+    @Test
+    fun testMultipleModules() {
+        runTest("testData/api/multipleModules.kt")
+    }
+
+    @TestMetadata("nestedClassType.kt")
+    @Test
+    fun testNestedClassType() {
+        runTest("testData/api/nestedClassType.kt")
+    }
+
+    @TestMetadata("nullableTypes.kt")
+    @Test
+    fun testNullableTypes() {
+        runTest("testData/api/nullableTypes.kt")
+    }
+
+    @TestMetadata("overridee.kt")
+    @Test
+    fun testOverridee() {
+        runTest("testData/api/overridee.kt")
+    }
+
+    @TestMetadata("parameterTypes.kt")
+    @Test
+    fun testParameterTypes() {
+        runTest("testData/api/parameterTypes.kt")
+    }
+
+    @TestMetadata("parent.kt")
+    @Test
+    fun testParent() {
+        runTest("testData/api/parent.kt")
+    }
+
+    @TestMetadata("platformDeclaration.kt")
+    @Test
+    fun testPlatformDeclaration() {
+        runTest("testData/api/platformDeclaration.kt")
+    }
+
+    @TestMetadata("rawTypes.kt")
+    @Test
+    fun testRawTypes() {
+        runTest("testData/api/rawTypes.kt")
+    }
+
+    @TestMetadata("recordJavaAnnotationTypes.kt")
+    @Test
+    fun testRecordJavaAnnotationTypes() {
+        runTest("testData/api/recordJavaAnnotationTypes.kt")
+    }
+
+    @TestMetadata("recordJavaAsMemberOf.kt")
+    @Test
+    fun testRecordJavaAsMemberOf() {
+        runTest("testData/api/recordJavaAsMemberOf.kt")
+    }
+
+    @TestMetadata("recordJavaGetAllMembers.kt")
+    @Test
+    fun testRecordJavaGetAllMembers() {
+        runTest("testData/api/recordJavaGetAllMembers.kt")
+    }
+
+    @TestMetadata("recordJavaOverrides.kt")
+    @Test
+    fun testRecordJavaOverrides() {
+        runTest("testData/api/recordJavaOverrides.kt")
+    }
+
+    @TestMetadata("recordJavaSupertypes.kt")
+    @Test
+    fun testRecordJavaSupertypes() {
+        runTest("testData/api/recordJavaSupertypes.kt")
+    }
+
+    @TestMetadata("referenceElement.kt")
+    @Test
+    fun testReferenceElement() {
+        runTest("testData/api/referenceElement.kt")
+    }
+
+    @TestMetadata("replaceWithErrorTypeArgs.kt")
+    @Test
+    fun testReplaceWithErrorTypeArgs() {
+        runTest("testData/api/replaceWithErrorTypeArgs.kt")
+    }
+
+    @TestMetadata("resolveJavaType.kt")
+    @Test
+    fun testResolveJavaType() {
+        runTest("testData/api/resolveJavaType.kt")
+    }
+
+    @TestMetadata("sealedClass.kt")
+    @Test
+    fun testSealedClass() {
+        runTest("testData/api/sealedClass.kt")
+    }
+
+    @TestMetadata("signatureMapper.kt")
+    @Test
+    fun testSignatureMapper() {
+        runTest("testData/api/signatureMapper.kt")
+    }
+
+    @TestMetadata("throwList.kt")
+    @Test
+    fun testThrowList() {
+        runTest("testData/api/throwList.kt")
+    }
+
+    @TestMetadata("topLevelMembers.kt")
+    @Test
+    fun testTopLevelMembers() {
+        runTest("testData/api/topLevelMembers.kt")
+    }
+
+    @TestMetadata("typeAlias.kt")
+    @Test
+    fun testTypeAlias() {
+        runTest("testData/api/typeAlias.kt")
+    }
+
+    @TestMetadata("typeAliasComparison.kt")
+    @Test
+    fun testTypeAliasComparison() {
+        runTest("testData/api/typeAliasComparison.kt")
+    }
+
+    @TestMetadata("typeComposure.kt")
+    @Test
+    fun testTypeComposure() {
+        runTest("testData/api/typeComposure.kt")
+    }
+
+    @TestMetadata("typeParameterReference.kt")
+    @Test
+    fun testTypeParameterReference() {
+        runTest("testData/api/typeParameterReference.kt")
+    }
+
+    @TestMetadata("varianceTypeCheck.kt")
+    @Test
+    fun testVarianceTypeCheck() {
+        runTest("testData/api/varianceTypeCheck.kt")
+    }
+
+    @TestMetadata("validateTypes.kt")
+    @Test
+    fun testValidateTypes() {
+        runTest("testData/api/validateTypes.kt")
+    }
+
+    @TestMetadata("visibilities.kt")
+    @Test
+    fun testVisibilities() {
+        runTest("testData/api/visibilities.kt")
+    }
+}
