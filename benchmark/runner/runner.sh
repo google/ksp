12519af2062741827e4008686f38bf5d797f84fb
Jiaxiang Chen <jiaxiang@google.com>
2022-03-18 12:04:20 -0700

    * Dump compile time dependencies * Run benchmark from command line
    (cherry picked from commit f15c75346ec6dca9987ad23f35d571a64ac14c9d)

--- benchmark/runner/runner.sh
+++ benchmark/runner/runner.sh
@@ -1,12 +1,49 @@
-cd ../exhaustive-processor
-./gradlew :processor:publishToMavenLocal
-cd ..
-mkdir tmp
-cd tmp
-git clone https://github.com/inorichi/tachiyomi.git
-cd tachiyomi
-git checkout 938339690eecdfe309d83264b6a89aff3c767687
-git apply ../../runner/tachi.patch
-#./gradlew :app:kspDevDebugKotlin
-#cd ../..
-#rm -rf tmp
+#!/bin/bash
+/*
+ * Copyright 2022 Google LLC
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+set -e
+BENCHMARK_DIR=$1
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+cd $SCRIPT_DIR
+CP=../build/libs/benchmark.jar:$(echo lib/*.jar | tr ' ' ':')
+KSP_PLUGIN_ID=com.google.devtools.ksp.symbol-processing
+KSP_PLUGIN_OPT=plugin:$KSP_PLUGIN_ID
+
+KSP_PLUGIN_JAR=./com/google/devtools/ksp/symbol-processing-cmdline/2.0.255/symbol-processing-cmdline-2.0.255.jar
+KSP_API_JAR=./com/google/devtools/ksp/symbol-processing-api/2.0.255/symbol-processing-api-2.0.255.jar
+
+AP=processor-1.0-SNAPSHOT.jar
+
+mkdir -p out
+find $SCRIPT_DIR/../$BENCHMARK_DIR -name "*.kt" | xargs java -cp $CP com.google.devtools.ksp.BenchRunnerKt \
+        -kotlin-home . \
+        -Xplugin=$KSP_PLUGIN_JAR \
+        -Xplugin=$KSP_API_JAR \
+        -Xallow-no-source-files \
+        -P $KSP_PLUGIN_OPT:apclasspath=$AP \
+        -P $KSP_PLUGIN_OPT:projectBaseDir=. \
+        -P $KSP_PLUGIN_OPT:classOutputDir=./out \
+        -P $KSP_PLUGIN_OPT:javaOutputDir=./out \
+        -P $KSP_PLUGIN_OPT:kotlinOutputDir=./out \
+        -P $KSP_PLUGIN_OPT:resourceOutputDir=./out \
+        -P $KSP_PLUGIN_OPT:kspOutputDir=./out \
+        -P $KSP_PLUGIN_OPT:cachesDir=./out \
+        -P $KSP_PLUGIN_OPT:incremental=false \
+        -P $KSP_PLUGIN_OPT:apoption=key1=value1 \
+        -P $KSP_PLUGIN_OPT:apoption=key2=value2 \
+        -cp $CP
