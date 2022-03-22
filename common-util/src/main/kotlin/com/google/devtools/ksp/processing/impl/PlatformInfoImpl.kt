edcf8002ce06b13537a04b4e8d39a8684c637458
Jiaxiang Chen <jiaxiang@google.com>
2022-03-21 22:41:44 -0700

    * Extract CodeGeneratorImpl, PlatformInfoImpl into common-util module, add copyright to common-util module classes. * Add KSP Command line processor and logger. * Implement KSP main execution and initialization logic.
    (cherry picked from commit 6c906beef9720a431343914c523d700af9c510a6)

--- common-util/src/main/kotlin/com/google/devtools/ksp/processing/impl/PlatformInfoImpl.kt
+++ common-util/src/main/kotlin/com/google/devtools/ksp/processing/impl/PlatformInfoImpl.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2022 Google LLC
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.devtools.ksp.processing.impl
 
 import com.google.devtools.ksp.processing.JsPlatformInfo
@@ -5,27 +22,27 @@ import com.google.devtools.ksp.processing.JvmPlatformInfo
 import com.google.devtools.ksp.processing.NativePlatformInfo
 import com.google.devtools.ksp.processing.UnknownPlatformInfo
 
-internal class JvmPlatformInfoImpl(
+class JvmPlatformInfoImpl(
     override val platformName: String,
     override val jvmTarget: String
 ) : JvmPlatformInfo {
     override fun toString() = "$platformName ($jvmTarget)"
 }
 
-internal class JsPlatformInfoImpl(
+class JsPlatformInfoImpl(
     override val platformName: String
 ) : JsPlatformInfo {
     override fun toString() = platformName
 }
 
-internal class NativePlatformInfoImpl(
+class NativePlatformInfoImpl(
     override val platformName: String,
     override val targetName: String
 ) : NativePlatformInfo {
     override fun toString() = "$platformName ($targetName)"
 }
 
-internal class UnknownPlatformInfoImpl(
+class UnknownPlatformInfoImpl(
     override val platformName: String
 ) : UnknownPlatformInfo {
     override fun toString() = platformName
