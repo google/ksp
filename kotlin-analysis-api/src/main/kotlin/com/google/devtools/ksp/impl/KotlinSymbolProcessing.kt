d11347d57279633534a522c5a274f4b2c0a333a7
Jiaxiang Chen <jiaxiang@google.com>
2022-03-21 22:41:44 -0700

    * Extract CodeGeneratorImpl, PlatformInfoImpl into common-util module, add copyright to common-util module classes. * Add KSP Command line processor and logger. * Implement KSP main execution and initialization logic.

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/KotlinSymbolProcessing.kt
@@ -1,5 +1,12 @@
 package com.google.devtools.ksp.impl
 
+import com.google.devtools.ksp.AnyChanges
+import com.google.devtools.ksp.KspOptions
+import com.google.devtools.ksp.impl.symbol.kotlin.KSFileImpl
+import com.google.devtools.ksp.processing.*
+import com.google.devtools.ksp.processing.impl.CodeGeneratorImpl
+import com.google.devtools.ksp.processing.impl.JvmPlatformInfoImpl
+import com.google.devtools.ksp.symbol.KSAnnotated
 import com.intellij.mock.MockApplication
 import com.intellij.mock.MockProject
 import com.intellij.openapi.application.ApplicationManager
@@ -8,66 +15,112 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.StandardFileSystems
 import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
 import com.intellij.psi.PsiManager
-import org.jetbrains.kotlin.analysis.api.analyseWithReadAction
 import org.jetbrains.kotlin.analysis.api.standalone.configureApplicationEnvironment
 import org.jetbrains.kotlin.analysis.api.standalone.configureProjectEnvironment
-import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
-import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoots
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.common.config.kotlinSourceRoots
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.plugins.ServiceLoaderLite
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.psi.KtFile
-import java.io.File
-import java.nio.file.Files
+import java.net.URLClassLoader
 
-internal fun convertFilesToKtFiles(project: Project, files: List<File>): List<KtFile> {
-    val fs = StandardFileSystems.local()
-    val psiManager = PsiManager.getInstance(project)
-    val ktFiles = mutableListOf<KtFile>()
-    for (file in files) {
-        val vFile = fs.findFileByPath(file.absolutePath) ?: continue
-        val ktFile = psiManager.findFile(vFile) as? KtFile ?: continue
-        ktFiles.add(ktFile)
-    }
-    return ktFiles
-}
+class KotlinSymbolProcessing(
+    val compilerConfiguration: CompilerConfiguration,
+    val options: KspOptions,
+    val logger: KSPLogger,
+    val testProcessor: SymbolProcessorProvider? = null
+) {
 
-fun main(args: Array<String>) {
-    val kotlinSourceRoots = args.toList().map { File(it) }
-    val compilerConfiguration = CompilerConfiguration()
-    compilerConfiguration.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-    val env = KotlinCoreEnvironment.createForProduction(
+    val providers: List<SymbolProcessorProvider>
+    val env: KotlinCoreEnvironment = KotlinCoreEnvironment.createForProduction(
         Disposer.newDisposable(), compilerConfiguration,
         EnvironmentConfigFiles.JVM_CONFIG_FILES
     )
-
-    val application = ApplicationManager.getApplication() as MockApplication
-    configureApplicationEnvironment(application)
-
-    val files = kotlinSourceRoots
-        .sortedBy { Files.isSymbolicLink(it.toPath()) } // Get non-symbolic paths first
-        .flatMap { root -> root.walk().filter { it.isFile && it.extension == "kt" }.toList() }
-        .sortedBy { Files.isSymbolicLink(it.toPath()) } // This time is for .java files
-        .distinctBy { it.canonicalPath }
-    compilerConfiguration.addKotlinSourceRoots(files.map { it.absolutePath })
-
     val project = env.project as MockProject
-    val ktFiles = convertFilesToKtFiles(project, files)
-    configureProjectEnvironment(
-        project,
-        compilerConfiguration,
-        env::createPackagePartProvider,
-        env.projectEnvironment.environment.jarFileSystem as CoreJarFileSystem
-    )
     val kspCoreEnvironment = KSPCoreEnvironment(project)
 
-    for (ktFile in ktFiles) {
-        analyseWithReadAction(ktFile) {
-            val fileSymbol = ktFile.getFileSymbol()
-            val members = fileSymbol.getFileScope().getAllSymbols()
-            members.filterIsInstance<KtFunctionSymbol>()
+    var finished = false
+    val deferredSymbols = mutableMapOf<SymbolProcessor, List<KSAnnotated>>()
+    val ktFiles = convertFilesToKtFiles(project, compilerConfiguration.kotlinSourceRoots.map { it.path })
+    val codeGenerator: CodeGeneratorImpl
+    val processors: List<SymbolProcessor>
+
+    init {
+        configureProjectEnvironment(
+            project,
+            compilerConfiguration,
+            env::createPackagePartProvider,
+            env.projectEnvironment.environment.jarFileSystem as CoreJarFileSystem
+        )
+
+        val ksFiles = ktFiles.map { KSFileImpl(it) }
+        val anyChangesWildcard = AnyChanges(options.projectBaseDir)
+        codeGenerator = CodeGeneratorImpl(
+            options.classOutputDir,
+            options.javaOutputDir,
+            options.kotlinOutputDir,
+            options.resourceOutputDir,
+            options.projectBaseDir,
+            anyChangesWildcard,
+            ksFiles,
+            options.incremental
+        )
+        val application = ApplicationManager.getApplication() as MockApplication
+        configureApplicationEnvironment(application)
+        providers = if (testProcessor != null) {
+            listOf(testProcessor)
+        } else {
+            val processingClasspath = options.processingClasspath
+            val classLoader =
+                URLClassLoader(processingClasspath.map { it.toURI().toURL() }.toTypedArray(), javaClass.classLoader)
+
+            ServiceLoaderLite.loadImplementations(SymbolProcessorProvider::class.java, classLoader)
+        }
+
+        processors = providers.mapNotNull { provider ->
+            var processor: SymbolProcessor? = null
+            processor = provider.create(
+                SymbolProcessorEnvironment(
+                    options.processingOptions,
+                    options.languageVersion,
+                    codeGenerator,
+                    logger,
+                    options.apiVersion,
+                    options.compilerVersion,
+                    // TODO: fix platform info
+                    listOf(JvmPlatformInfoImpl("JVM", "1.8"))
+                )
+            )
+            processor.also { deferredSymbols[it] = mutableListOf() }
         }
     }
+
+    fun execute() {
+        val resolver = ResolverAAImpl(ktFiles)
+        processors.forEach { it.process(resolver) }
+    }
+
+    private fun convertFilesToKtFiles(project: Project, filePaths: List<String>): List<KtFile> {
+        val fs = StandardFileSystems.local()
+        val psiManager = PsiManager.getInstance(project)
+        val ktFiles = mutableListOf<KtFile>()
+        for (path in filePaths) {
+            val vFile = fs.findFileByPath(path) ?: continue
+            val ktFile = psiManager.findFile(vFile) as? KtFile ?: continue
+            ktFiles.add(ktFile)
+        }
+        return ktFiles
+    }
+}
+
+fun main(args: Array<String>) {
+    val commandLineProcessor = KSPCommandLineProcessor(args)
+    val logger = CommandLineKSPLogger()
+    val kotlinSymbolProcessing = KotlinSymbolProcessing(
+        commandLineProcessor.compilerConfiguration,
+        commandLineProcessor.kspOptions,
+        logger
+    )
+    kotlinSymbolProcessing.execute()
 }
