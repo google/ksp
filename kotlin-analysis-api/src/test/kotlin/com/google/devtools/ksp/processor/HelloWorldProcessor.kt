9f5f322b67b0d17d2df68252e7d960a360e2e467
Jiaxiang Chen <jiaxiang@google.com>
2022-03-23 14:12:30 -0700

    Use AlwaysAccessibleValidityToken for KtSymbol analysis

--- kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/processor/HelloWorldProcessor.kt
+++ kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/processor/HelloWorldProcessor.kt
@@ -9,31 +9,9 @@ class HelloWorldProcessor : AbstractTestProcessor() {
     override fun process(resolver: Resolver): List<KSAnnotated> {
         val fooKt = resolver.getAllFiles().single()
         results.add(fooKt.fileName)
-
-        // FIXME: symbols escape contexts ¯\_(ツ)_/¯
-        // This doesn't work; AA complains "... is inaccessible: Called outside analyse method"
-        /*
         val bar = resolver.getAllFiles().single { it.fileName == "Foo.kt" }
             .declarations.single { it.simpleName.asString() == "Foo" }
-            .safeAs<KSClassDeclaration>()!!.superTypes.single().resolve().declaration
-            .safeAs<KSClassDeclaration>()!!
-
         results.add(bar.simpleName.asString())
-         */
-
-        // This is OK if making KSFileImpl.ktFile public:
-        /*
-        val ktFile = fooKt.safeAs<KSFileImpl>()!!.ktFile
-        analyseWithReadAction(ktFile) {
-            val fileSymbol = ktFile.getFileSymbol()
-            val members = fileSymbol.getFileScope().getAllSymbols()
-            val classes = members.filterIsInstance<KtClassOrObjectSymbol>()
-            classes.first().superTypes.filter { it.toString() == "Bar" }.firstOrNull()?.let {
-                results.add(it.toString())
-            }
-        }
-         */
-
         return emptyList()
     }
 
