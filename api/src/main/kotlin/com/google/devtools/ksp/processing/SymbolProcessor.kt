d6a1b0fbac0a0038db57f9d98dfe5fb91525643f
Maxr1998 <max.rumpf1998@gmail.com>
2022-04-05 13:28:45 +0200

    Fix doc typos and improve wording
    (cherry picked from commit 7bb7a5637ba559eb549e5ddc4a69c35b186f5328)

--- api/src/main/kotlin/com/google/devtools/ksp/processing/SymbolProcessor.kt
+++ api/src/main/kotlin/com/google/devtools/ksp/processing/SymbolProcessor.kt
@@ -20,10 +20,10 @@ import com.google.devtools.ksp.symbol.KSAnnotated
 
 /**
  * [SymbolProcessor] is the interface used by plugins to integrate into Kotlin Symbol Processing.
- * SymbolProcessor supports multiple round execution, a processor may return a list of deferred symbols at the end
+ * SymbolProcessor supports multiple rounds of execution, a processor may return a list of deferred symbols at the end
  * of every round, which will be passed to processors again in the next round, together with the newly generated symbols.
- * Upon Exceptions, KSP will try to distinguish the exceptions from KSP and exceptions from processors.
- * Exceptions will result in a termination of processing immediately and be logged as an error in KSPLogger.
+ * On exceptions, KSP will try to distinguish between exceptions from KSP and exceptions from processors.
+ * Exceptions from processors will immediately terminate processing and be logged as an error in KSPLogger.
  * Exceptions from KSP should be reported to KSP developers for further investigation.
  * At the end of the round where exceptions or errors happened, all processors will invoke onError() function to do
  * their own error handling.
