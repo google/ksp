760f2f052fefb52a8f1b53e52065adc39c97a495
Jiaxiang Chen <jiaxiang@google.com>
2022-06-02 11:34:46 -0700

    replace deprecated compatibility jvm-default flag with all-compatibility

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,7 +8,7 @@ val signingPassword: String? by project
 val kotlinBaseVersion: String by project
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.freeCompilerArgs += "-Xjvm-default=compatibility"
+    kotlinOptions.freeCompilerArgs += "-Xjvm-default=all-compatibility"
 }
 
 plugins {
