c6eb80a6571c44c8542e0d8867846f51784ebe6e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-09-19 11:43:10 +0200

    Enable kotlin-analysis-api module back

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ include("compiler-plugin")
 include("symbol-processing")
 include("symbol-processing-cmdline")
 include("integration-tests")
+include("kotlin-analysis-api")
 
 val kotlinProjectPath: String? by settings
 if (kotlinProjectPath != null) {
