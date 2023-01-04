b7280295bc830a15020c6fdf87347c087dc7fcdf
Ting-Yuan Huang <laszio@google.com>
2023-01-03 17:28:09 -0800

    Build KSP with Kotlin 1.8.0
    instead of the bootstrapping version

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -3,12 +3,4 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
     }
-    val kotlinBaseVersion: String = java.util.Properties().also { props ->
-        settingsDir.parentFile.resolve("gradle.properties").inputStream().use {
-            props.load(it)
-        }
-    }["kotlinBaseVersion"] as String
-    plugins {
-        id("org.jetbrains.kotlin.jvm") version kotlinBaseVersion
-    }
 }
