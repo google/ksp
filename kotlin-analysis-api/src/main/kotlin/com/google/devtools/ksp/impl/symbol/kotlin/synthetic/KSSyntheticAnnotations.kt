0038f41dcbc2fcc6149fe8a27e028f251ac4f542
Jiaxiang Chen <jiaxiang@google.com>
2023-03-13 15:08:50 -0700

    Revert "UPDATE_KOTLIN_VERSION: 1.9.0-dev-1777"
    This reverts commit f3043917af405590e9c8d9422047fb1e3aedfe6e.

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/synthetic/KSSyntheticAnnotations.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/synthetic/KSSyntheticAnnotations.kt
@@ -1,12 +1,11 @@
 package com.google.devtools.ksp.impl.symbol.kotlin.synthetic
 
-import org.jetbrains.kotlin.analysis.api.annotations.KtAnnotationApplicationWithArgumentsInfo
+import org.jetbrains.kotlin.analysis.api.annotations.KtAnnotationApplication
 import org.jetbrains.kotlin.name.ClassId
 
-fun getExtensionFunctionTypeAnnotation(index: Int) = KtAnnotationApplicationWithArgumentsInfo(
+val ExtensionFunctionTypeAnnotation = KtAnnotationApplication(
     ClassId.fromString(ExtensionFunctionType::class.qualifiedName!!),
     null,
     null,
-    emptyList(),
-    index
+    emptyList()
 )
