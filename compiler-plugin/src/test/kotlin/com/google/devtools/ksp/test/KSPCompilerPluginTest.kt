136f4be50b8f2e7345b3006f18f97648f29956bd
Jiaxiang Chen <jiaxiang@google.com>
2023-07-06 11:39:26 -0700

    Add Resolver.getPackageAnnotations Resolver.getPackagesWithAnnotation api. These new APIs provides capability to inspect java package level annotations.

--- compiler-plugin/src/test/kotlin/com/google/devtools/ksp/test/KSPCompilerPluginTest.kt
+++ compiler-plugin/src/test/kotlin/com/google/devtools/ksp/test/KSPCompilerPluginTest.kt
@@ -387,6 +387,12 @@ class KSPCompilerPluginTest : AbstractKSPCompilerPluginTest() {
         runTest("../test-utils/testData/api/overridee.kt")
     }
 
+    @TestMetadata("packageAnnotations.kt")
+    @Test
+    fun testPackageAnnotation() {
+        runTest("../test-utils/testData/api/packageAnnotations.kt")
+    }
+
     @TestMetadata("parameterTypes.kt")
     @Test
     fun testParameterTypes() {
