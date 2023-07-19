126552fa29b17ffd6512da291375f3e56c1412b5
Gasper Kojek <gkojek@gradle.com>
2023-07-19 15:59:30 +0200

    Fixed integration-tests:test task caching

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -1,4 +1,4 @@
-import com.google.devtools.ksp.RelativizingPathProvider
+import com.google.devtools.ksp.RelativizingInternalPathProvider
 
 val junitVersion: String by project
 val kotlinBaseVersion: String by project
@@ -12,13 +12,17 @@ dependencies {
     testImplementation("junit:junit:$junitVersion")
     testImplementation(gradleTestKit())
     testImplementation("org.jetbrains.kotlin:kotlin-compiler:$kotlinBaseVersion")
+    testImplementation(project(":api"))
+    testImplementation(project(":gradle-plugin"))
+    testImplementation(project(":symbol-processing"))
+    testImplementation(project(":symbol-processing-cmdline"))
 }
 
 tasks.named<Test>("test") {
     systemProperty("kotlinVersion", kotlinBaseVersion)
     systemProperty("kspVersion", version)
     systemProperty("agpVersion", agpBaseVersion)
-    jvmArgumentProviders.add(RelativizingPathProvider("testRepo", File(rootProject.buildDir, "repos/test")))
+    jvmArgumentProviders.add(RelativizingInternalPathProvider("testRepo", File(rootProject.buildDir, "repos/test")))
     dependsOn(":api:publishAllPublicationsToTestRepository")
     dependsOn(":gradle-plugin:publishAllPublicationsToTestRepository")
     dependsOn(":symbol-processing:publishAllPublicationsToTestRepository")
