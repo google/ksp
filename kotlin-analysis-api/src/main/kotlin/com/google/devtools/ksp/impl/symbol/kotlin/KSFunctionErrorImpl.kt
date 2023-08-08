50a5275462fd89daef4f8a431a2bdb9a91a2f18c
Jiaxiang Chen <jiaxiang@google.com>
2023-08-08 11:12:26 -0700

    implement asMemberOf

---
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSFunctionErrorImpl.kt
@@ -0,0 +1,41 @@
+package com.google.devtools.ksp.impl.symbol.kotlin
+
+import com.google.devtools.ksp.symbol.KSFunction
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
+import com.google.devtools.ksp.symbol.KSType
+import com.google.devtools.ksp.symbol.KSTypeParameter
+
+class KSFunctionErrorImpl(
+    private val declaration: KSFunctionDeclaration
+) : KSFunction {
+    override val isError: Boolean = true
+
+    override val returnType: KSType = KSErrorType
+
+    override val parameterTypes: List<KSType?>
+        get() = declaration.parameters.map {
+            KSErrorType
+        }
+    override val typeParameters: List<KSTypeParameter>
+        get() = emptyList()
+
+    override val extensionReceiverType: KSType?
+        get() = declaration.extensionReceiver?.let {
+            KSErrorType
+        }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as KSFunctionErrorImpl
+
+        if (declaration != other.declaration) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return declaration.hashCode()
+    }
+}
