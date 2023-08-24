eeb1cb7e22ce570248eac2159ac2eb6e111f28e8
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-08-24 14:49:36 +0200

    UPDATE_KOTLIN_VERSION: 1.9.30-dev-2548 Kotlin: Migrate from ChangedFiles to SourcesChanges

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KotlinFactories.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KotlinFactories.kt
@@ -40,6 +40,7 @@ import org.gradle.process.CommandLineArgumentProvider
 import org.gradle.process.ExecOperations
 import org.gradle.work.InputChanges
 import org.gradle.workers.WorkerExecutor
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
 import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
@@ -57,7 +58,6 @@ import org.jetbrains.kotlin.gradle.tasks.TaskOutputsBackup
 import org.jetbrains.kotlin.gradle.tasks.configuration.BaseKotlin2JsCompileConfig
 import org.jetbrains.kotlin.gradle.tasks.configuration.KotlinCompileCommonConfig
 import org.jetbrains.kotlin.gradle.tasks.configuration.KotlinCompileConfig
-import org.jetbrains.kotlin.incremental.ChangedFiles
 import org.jetbrains.kotlin.konan.target.HostManager
 import java.io.File
 import java.nio.file.Paths
@@ -175,7 +175,7 @@ interface KspTask : Task {
     val commandLineArgumentProviders: ListProperty<CommandLineArgumentProvider>
 
     @get:Internal
-    val incrementalChangesTransformers: ListProperty<(ChangedFiles) -> List<SubpluginOption>>
+    val incrementalChangesTransformers: ListProperty<(SourcesChanges) -> List<SubpluginOption>>
 }
 
 @CacheableTask
