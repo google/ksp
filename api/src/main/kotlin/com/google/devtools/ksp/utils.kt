c05d96d75d087b1a35bc75ee40d6b97b5622a951
seiko <seiko_des@outlook.com>
2023-09-08 12:11:55 +0800

    cache array result if result type is same with method return type

--- api/src/main/kotlin/com/google/devtools/ksp/utils.kt
+++ api/src/main/kotlin/com/google/devtools/ksp/utils.kt
@@ -377,9 +377,11 @@ private fun KSAnnotation.createInvocationHandler(clazz: Class<*>): InvocationHan
                             if (result !is Array<*>) {
                                 val value = { result.asArray(method, clazz) }
                                 cache.getOrPut(Pair(method.returnType, value), value)
-                            } else {
+                            } else if (method.returnType == result::class.java) {
                                 val value = { result }
                                 cache.getOrPut(Pair(method.returnType, value), value)
+                            } else {
+                                throw IllegalStateException("unhandled value type, $ExceptionMessage")
                             }
                         }
                         method.returnType.isEnum -> {
