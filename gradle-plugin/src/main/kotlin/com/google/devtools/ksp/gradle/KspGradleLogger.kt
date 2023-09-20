362c6f389980c8d001c023d7376859632372de9b
Ting-Yuan Huang <laszio@google.com>
2023-09-20 00:30:59 -0700

    KSP 2: support log level
    (cherry picked from commit 5fa0238e924908800d1b0c7a64271a586678bb6c)

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspGradleLogger.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspGradleLogger.kt
@@ -2,28 +2,33 @@ package com.google.devtools.ksp.gradle
 
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.symbol.KSNode
+import org.gradle.api.logging.LogLevel
 
-class KspGradleLogger : KSPLogger {
-    // TODO: support logging level.
+class KspGradleLogger(val loglevel: LogLevel) : KSPLogger {
     private val messager = System.out
 
     override fun logging(message: String, symbol: KSNode?) {
-        messager.println("v: [ksp] $message")
+        if (loglevel.ordinal <= LogLevel.DEBUG.ordinal)
+            messager.println("v: [ksp] $message")
     }
 
     override fun info(message: String, symbol: KSNode?) {
-        messager.println("i: [ksp] $message")
+        if (loglevel.ordinal <= LogLevel.INFO.ordinal)
+            messager.println("i: [ksp] $message")
     }
 
     override fun warn(message: String, symbol: KSNode?) {
-        messager.println("w: [ksp] $message")
+        if (loglevel.ordinal <= LogLevel.WARN.ordinal)
+            messager.println("w: [ksp] $message")
     }
 
     override fun error(message: String, symbol: KSNode?) {
-        messager.println("e: [ksp] $message")
+        if (loglevel.ordinal <= LogLevel.ERROR.ordinal)
+            messager.println("e: [ksp] $message")
     }
 
     override fun exception(e: Throwable) {
-        messager.println("e: [ksp] $e")
+        if (loglevel.ordinal <= LogLevel.ERROR.ordinal)
+            messager.println("e: [ksp] $e")
     }
 }
