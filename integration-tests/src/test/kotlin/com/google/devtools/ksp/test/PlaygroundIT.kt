4d84d6e7141d70181e5e2a7658eafa746c58f4ee
Jiaxiang Chen <jiaxiang@google.com>
2023-10-09 14:32:00 -0700

    disable 2 tests not working due to compiler version mismatch in release branch

--- integration-tests/src/test/kotlin/com/google/devtools/ksp/test/PlaygroundIT.kt
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/PlaygroundIT.kt
@@ -270,6 +270,7 @@ class PlaygroundIT(val useK2: Boolean) {
 
     @Test
     fun testVersions() {
+        Assume.assumeFalse(useK2)
         val kotlinCompile = "org.jetbrains.kotlin.gradle.tasks.KotlinCompile"
         val buildFile = File(project.root, "workload/build.gradle.kts")
         buildFile.appendText("\ntasks.withType<$kotlinCompile> {")
@@ -332,6 +333,7 @@ class PlaygroundIT(val useK2: Boolean) {
 
     @Test
     fun testProjectExtensionCompilerOptions() {
+        Assume.assumeFalse(useK2)
         Assume.assumeFalse(System.getProperty("os.name").startsWith("Windows", ignoreCase = true))
         val properties = File(project.root, "gradle.properties")
         properties.writeText(
