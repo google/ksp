83b274e399f7381131eda1b59ae2c3dae289d3cc
Ting-Yuan Huang <laszio@google.com>
2023-10-17 14:22:48 -0700

    UPDATE_KOTLIN_VERSION: 2.0.0-dev-4562
    (cherry picked from commit 4e408831e64de9fd62c2220c4a3f5fa46a6d89a6)

--- integration-tests/src/test/kotlin/com/google/devtools/ksp/test/KSPCmdLineOptionsIT.kt
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/KSPCmdLineOptionsIT.kt
@@ -38,6 +38,7 @@ class KSPCmdLineOptionsIT(val useKSP2: Boolean) {
         }.maxByOrNull { it.lastModified() }!!
         val compilerArgs = mutableListOf(
             "-no-stdlib",
+            "-language-version", "1.9",
             "-Xplugin=${kspPluginJar.absolutePath}",
             "-Xplugin=${kspApiJar.absolutePath}",
             "-P", "plugin:$kspPluginId:apclasspath=${processorJar.absolutePath}",
