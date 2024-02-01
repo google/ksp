f7b15c3b2d776eb73aa92f83de647701095d8c44
Jiaxiang Chen <jiaxiang@google.com>
2024-01-31 17:57:50 -0800

    implement getJavaWildcard for KSP2
    (cherry picked from commit c56396b51282b311e2bf81426599e3b12ff172e8)

--- compiler-plugin/src/main/kotlin/com/google/devtools/ksp/processing/impl/ResolverImpl.kt
+++ compiler-plugin/src/main/kotlin/com/google/devtools/ksp/processing/impl/ResolverImpl.kt
@@ -28,6 +28,10 @@ import com.google.devtools.ksp.common.JVM_VOLATILE_ANNOTATION_FQN
 import com.google.devtools.ksp.common.extractThrowsAnnotation
 import com.google.devtools.ksp.common.impl.KSNameImpl
 import com.google.devtools.ksp.common.impl.KSTypeReferenceSyntheticImpl
+import com.google.devtools.ksp.common.impl.RefPosition
+import com.google.devtools.ksp.common.impl.findOuterMostRef
+import com.google.devtools.ksp.common.impl.findRefPosition
+import com.google.devtools.ksp.common.impl.isReturnTypeOfAnnotationMethod
 import com.google.devtools.ksp.common.javaModifiers
 import com.google.devtools.ksp.common.memoized
 import com.google.devtools.ksp.common.visitor.CollectAnnotatedSymbolsVisitor
@@ -1230,67 +1234,6 @@ class ResolverImpl(
             else -> Unit
         }
         return modifiers
-    }
-
-    private enum class RefPosition {
-        PARAMETER_TYPE,
-        RETURN_TYPE,
-        SUPER_TYPE
-    }
-
-    // Search in self and parents for the first type reference that is not part of a type argument.
-    private fun KSTypeReference.findOuterMostRef(): Pair<KSTypeReference, List<Int>> {
-        fun KSNode.findParentRef(): KSTypeReference? {
-            var parent = parent
-            while (parent != null && parent !is KSTypeReference)
-                parent = parent.parent
-            return parent as? KSTypeReference
-        }
-
-        val fallback = Pair<KSTypeReference, List<Int>>(this, emptyList())
-        val indexes = mutableListOf<Int>()
-        var candidate: KSTypeReference = this
-        // KSTypeArgument's parent can be either KSReferenceElement or KSType.
-        while (candidate.parent is KSTypeArgument) {
-            // If the parent is a KSType, it's a synthetic reference.
-            // Do nothing and reply on the fallback behavior.
-            val referenceElement = (candidate.parent!!.parent as? KSReferenceElement) ?: return fallback
-            indexes.add(referenceElement.typeArguments.indexOf(candidate.parent))
-            // In case the program isn't properly structured, fallback.
-            candidate = referenceElement.findParentRef() ?: return fallback
-        }
-        return Pair(candidate, indexes)
-    }
-
-    // TODO: Strict mode for catching unhandled cases.
-    private fun findRefPosition(ref: KSTypeReference): RefPosition = when (val parent = ref.parent) {
-        is KSCallableReference -> when (ref) {
-            parent.returnType -> RefPosition.RETURN_TYPE
-            else -> RefPosition.PARAMETER_TYPE
-        }
-        is KSFunctionDeclaration -> when (ref) {
-            parent.returnType -> RefPosition.RETURN_TYPE
-            else -> RefPosition.PARAMETER_TYPE
-        }
-        is KSPropertyGetter -> RefPosition.RETURN_TYPE
-        is KSPropertyDeclaration -> when (ref) {
-            parent.type -> RefPosition.RETURN_TYPE
-            else -> RefPosition.PARAMETER_TYPE
-        }
-        is KSClassDeclaration -> RefPosition.SUPER_TYPE
-        // is KSTypeArgument -> RefPosition.PARAMETER_TYPE
-        // is KSAnnotation -> RefPosition.PARAMETER_TYPE
-        // is KSTypeAlias -> RefPosition.PARAMETER_TYPE
-        // is KSValueParameter -> RefPosition.PARAMETER_TYPE
-        // is KSTypeParameter -> RefPosition.PARAMETER_TYPE
-        else -> RefPosition.PARAMETER_TYPE
-    }
-
-    private fun KSTypeReference.isReturnTypeOfAnnotationMethod(): Boolean {
-        var candidate = this.parent
-        while (candidate !is KSClassDeclaration && candidate != null)
-            candidate = candidate.parent
-        return (candidate as? KSClassDeclaration)?.classKind == ClassKind.ANNOTATION_CLASS
     }
 
     // Convert type arguments for Java wildcard, recursively.
