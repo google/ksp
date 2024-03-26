a1bc7231ca5aa6c84d4dbc102e679bf598107728
Ting-Yuan Huang <laszio@google.com>
2024-03-26 13:49:20 -0700

    Replace IdKey's impl with identityHashCode
    (cherry picked from commit 4b6fe0ec85ab09f0334245fbefc222a5c3d3d006)

--- common-util/src/main/kotlin/com/google/devtools/ksp/common/KSPUtils.kt
+++ common-util/src/main/kotlin/com/google/devtools/ksp/common/KSPUtils.kt
@@ -3,21 +3,23 @@ package com.google.devtools.ksp.common
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSType
 
+private inline fun Any?.idHashCode() = System.identityHashCode(this)
+
 class IdKey<T>(private val k: T) {
     override fun equals(other: Any?): Boolean = if (other is IdKey<*>) k === other.k else false
-    override fun hashCode(): Int = k.hashCode()
+    override fun hashCode(): Int = k.idHashCode()
 }
 
 class IdKeyPair<T, P>(private val k1: T, private val k2: P) {
     override fun equals(other: Any?): Boolean = if (other is IdKeyPair<*, *>) k1 === other.k1 &&
         k2 === other.k2 else false
-    override fun hashCode(): Int = k1.hashCode() * 31 + k2.hashCode()
+    override fun hashCode(): Int = k1.idHashCode() * 31 + k2.idHashCode()
 }
 
 class IdKeyTriple<T, P, Q>(private val k1: T, private val k2: P, private val k3: Q) {
     override fun equals(other: Any?): Boolean = if (other is IdKeyTriple<*, *, *>) k1 === other.k1 &&
         k2 === other.k2 && k3 === other.k3 else false
-    override fun hashCode(): Int = k1.hashCode() * 31 * 31 + k2.hashCode() * 31 + k3.hashCode()
+    override fun hashCode(): Int = k1.idHashCode() * 31 * 31 + k2.idHashCode() * 31 + k3.idHashCode()
 }
 
 @SuppressWarnings("UNCHECKED_CAST")
