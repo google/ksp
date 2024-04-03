e66b0856c9a7737d67dab3d09cd01d2d978a5be1
Jiaxiang Chen <jiaxiang@google.com>
2024-04-02 17:59:43 -0700

    use proper scope for getting declared static memberscope
    (cherry picked from commit 88b78d4c912c4df34fbf7799bea2d02d660972c4)

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/util.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/util.kt
@@ -231,7 +231,7 @@ internal inline fun <R> analyze(crossinline action: KtAnalysisSession.() -> R): 
 internal fun KtSymbolWithMembers.declarations(): Sequence<KSDeclaration> {
     return analyze {
         this@declarations.let {
-            it.getDeclaredMemberScope().getAllSymbols() + it.getStaticMemberScope().getAllSymbols()
+            it.getDeclaredMemberScope().getAllSymbols() + it.getStaticDeclaredMemberScope().getAllSymbols()
         }.distinct().map { symbol ->
             when (symbol) {
                 is KtNamedClassOrObjectSymbol -> KSClassDeclarationImpl.getCached(symbol)
