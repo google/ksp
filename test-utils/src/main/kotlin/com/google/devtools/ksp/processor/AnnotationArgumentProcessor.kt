d432b52b8fa53bf212b02e80386dd306362bc666
Kuan-Ying Chou <kuanyingchou@google.com>
2024-04-11 14:57:28 +0100

    Test default annotation values
    (cherry picked from commit 207b8aad243bdd81d8a3165ed9b5b8b127274d5c)

--- test-utils/src/main/kotlin/com/google/devtools/ksp/processor/AnnotationArgumentProcessor.kt
+++ test-utils/src/main/kotlin/com/google/devtools/ksp/processor/AnnotationArgumentProcessor.kt
@@ -29,6 +29,14 @@ class AnnotationArgumentProcessor : AbstractTestProcessor() {
     val visitor = ArgumentVisitor()
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
+        listOf("MyClass", "MyClassInLib").forEach { clsName ->
+            resolver.getClassDeclarationByName(clsName)!!.let { cls ->
+                cls.annotations.single().arguments.forEach {
+                    println("${clsName}: ${it.name!!.asString()} = ${it.value}")
+                }
+            }
+        }
+
         resolver.getSymbolsWithAnnotation("Bar", true).forEach {
             it.annotations.forEach { it.arguments.forEach { it.accept(visitor, Unit) } }
         }
