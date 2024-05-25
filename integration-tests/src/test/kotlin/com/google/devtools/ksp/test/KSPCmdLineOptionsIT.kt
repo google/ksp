319e095d1ad61a89f621ba116ea2bd3d5b034372
Ting-Yuan Huang <laszio@google.com>
2024-05-24 17:18:49 -0700

    More tests for KSP2 command line tool

--- integration-tests/src/test/kotlin/com/google/devtools/ksp/test/KSPCmdLineOptionsIT.kt
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/KSPCmdLineOptionsIT.kt
@@ -12,6 +12,8 @@ import org.junit.runners.Parameterized
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.PrintStream
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import java.net.URLClassLoader
 
 data class CompileResult(val exitCode: ExitCode, val output: String)
@@ -89,6 +91,121 @@ class KSPCmdLineOptionsIT(val useKSP2: Boolean) {
             errors.any {
                 it.startsWith("error: [ksp] java.lang.IllegalStateException: Error on request")
             }
+        )
+    }
+
+    private fun getKsp2Main(mainClassName: String): Method {
+        val repoPath = "../build/repos/test/com/google/devtools/ksp/"
+
+        val commonDepsJar = File("$repoPath/symbol-processing-common-deps/2.0.255-SNAPSHOT").listFiles()!!.filter {
+            it.name.matches(Regex(".*-\\d.jar"))
+        }.maxByOrNull { it.lastModified() }!!
+        val kspMainJar = File("$repoPath/symbol-processing-aa-embeddable/2.0.255-SNAPSHOT").listFiles()!!.filter {
+            it.name.matches(Regex(".*-\\d.jar"))
+        }.maxByOrNull { it.lastModified() }!!
+        val kspApiJar = File("$repoPath/symbol-processing-api/2.0.255-SNAPSHOT").listFiles()!!.filter {
+            it.name.matches(Regex(".*-\\d.jar"))
+        }.maxByOrNull { it.lastModified() }!!
+
+        val kspClasspath = listOf(
+            commonDepsJar, kspMainJar, kspApiJar
+        ).map { it.toURI().toURL() }.toTypedArray()
+        val classLoader = URLClassLoader(kspClasspath)
+        val kspMainClass = classLoader.loadClass(mainClassName)
+
+        return kspMainClass.getMethod(
+            "main",
+            Array<String>::class.java,
+        )
+    }
+
+    private fun getKsp2SharedArgs(): List<String> {
+        val outDir = "${project.root.path}/build/out"
+        val srcDir = "${project.root.path}/workload/src/"
+
+        return listOf(
+            "-module-name=main",
+            "-project-base-dir", project.root.path,
+            "-source-roots", srcDir,
+            "-output-base-dir=$outDir",
+            "-caches-dir=$outDir",
+            "-class-output-dir=$outDir",
+            "-kotlin-output-dir=$outDir",
+            "-resource-output-dir", outDir,
+            "-language-version=2.0",
+            "-api-version=2.0",
+        )
+    }
+
+    fun testKsp2(mainClassName: String, platformArgs: List<String>) {
+        Assume.assumeTrue(useKSP2)
+
+        val sharedArgs = getKsp2SharedArgs()
+        val kspMain = getKsp2Main(mainClassName)
+
+        val gradleRunner = GradleRunner.create().withProjectDir(project.root)
+        gradleRunner.withArguments("clean", ":processors:build").build()
+        val processorJar = File(project.root, "processors/build/libs/processors-1.0-SNAPSHOT.jar").absolutePath
+
+        val outDir = "${project.root.path}/build/out"
+        val args = sharedArgs + platformArgs + listOf(processorJar)
+
+        kspMain.invoke(null, args.toTypedArray())
+
+        val status = File(outDir, "Status.log")
+        Assert.assertTrue(status.exists() && status.readText() == "OK")
+
+        val args2 = args + listOf("-processor-options", "error=true")
+        Assert.assertThrows(IllegalStateException::class.java) {
+            try {
+                kspMain.invoke(null, args2.toTypedArray())
+            } catch (e: InvocationTargetException) {
+                Assert.assertTrue(e.targetException is IllegalStateException)
+                Assert.assertTrue(e.targetException.message == "Error on request")
+                throw e.targetException
+            }
+        }
+    }
+
+    @Test
+    fun testKSPJvmMain() {
+        val outDir = "${project.root.path}/build/out"
+        testKsp2(
+            "com.google.devtools.ksp.cmdline.KSPJvmMain",
+            listOf(
+                "-java-output-dir", outDir,
+                "-jvm-target", "11",
+            )
+        )
+    }
+
+    @Test
+    fun testKSPCommonMain() {
+        testKsp2(
+            "com.google.devtools.ksp.cmdline.KSPCommonMain",
+            listOf(
+                "-targets=common",
+            )
+        )
+    }
+
+    @Test
+    fun testKSPJsMain() {
+        testKsp2(
+            "com.google.devtools.ksp.cmdline.KSPJsMain",
+            listOf(
+                "-backend=JS",
+            )
+        )
+    }
+
+    @Test
+    fun testKSPNativeMain() {
+        testKsp2(
+            "com.google.devtools.ksp.cmdline.KSPNativeMain",
+            listOf(
+                "-target=LinuxX64"
+            )
         )
     }
 
