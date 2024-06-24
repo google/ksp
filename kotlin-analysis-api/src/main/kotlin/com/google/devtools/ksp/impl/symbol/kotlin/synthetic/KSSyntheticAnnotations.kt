4aff517a5ae8cd84c1d4184ceaedc9c0e8cfe2d7
Ting-Yuan Huang <laszio@google.com>
2024-06-24 13:20:38 -0700

    fix ktlint errors
    (cherry picked from commit 4314f151bde2319548add6d4216c70c080755d2d)

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/synthetic/KSSyntheticAnnotations.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/synthetic/KSSyntheticAnnotations.kt
@@ -1,7 +1,6 @@
 package com.google.devtools.ksp.impl.symbol.kotlin.synthetic
 
 import com.google.devtools.ksp.impl.ResolverAAImpl
-import org.jetbrains.kotlin.analysis.api.annotations.KtAnnotationApplicationWithArgumentsInfo
 import org.jetbrains.kotlin.analysis.api.impl.base.annotations.KaAnnotationImpl
 import org.jetbrains.kotlin.analysis.api.platform.lifetime.KotlinAlwaysAccessibleLifetimeToken
 import org.jetbrains.kotlin.name.ClassId
