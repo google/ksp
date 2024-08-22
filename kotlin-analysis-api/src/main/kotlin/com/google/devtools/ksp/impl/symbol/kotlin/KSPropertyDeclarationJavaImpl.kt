39de553a1bb930f33a28c8b29a6ede866bd47f87
Ting-Yuan Huang <laszio@google.com>
2024-08-22 14:04:10 -0700

    Implement KSPropertyDeclarationJavaImpl.defer
    (cherry picked from commit e5872fbf4cbb2ae01ea00e0e9498e5ff40678049)

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSPropertyDeclarationJavaImpl.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/symbol/kotlin/KSPropertyDeclarationJavaImpl.kt
@@ -66,7 +66,7 @@ class KSPropertyDeclarationJavaImpl private constructor(val ktJavaFieldSymbol: K
     }
 
     override fun defer(): Restorable? {
-        TODO("Not yet implemented")
+        return ktJavaFieldSymbol.defer(::getCached)
     }
 }
 
