e19bc86ae20905b1ca77b067ed01414ad73efe4a
Kuan-Ying Chou <kuanyingchou@google.com>
2024-09-18 15:36:47 +0100

    Test defalut annotation value in Java

--- test-utils/testData/api/annotationValue_java2.kt
+++ test-utils/testData/api/annotationValue_java2.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright 2023 Google LLC
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2020 Google LLC
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,9 +21,12 @@
 // MyClass: MyAnnotation: stringParam = 2
 // MyClass: MyAnnotation: stringParam2 = 1
 // MyClass: MyAnnotation: stringArrayParam = [3, 5, 7]
-// MyClassInLib: MyAnnotation: stringParam = 2
-// MyClassInLib: MyAnnotation: stringParam2 = 1
-// MyClassInLib: MyAnnotation: stringArrayParam = [3, 5, 7]
+// MyClass: MyAnnotationInLib: stringParam = 2
+// MyClass: MyAnnotationInLib: stringParam2 = 1
+// MyClass: MyAnnotationInLib: stringArrayParam = [3, 5, 7]
+// MyClassInLib: MyAnnotationInLib: stringParam = 2
+// MyClassInLib: MyAnnotationInLib: stringParam2 = 1
+// MyClassInLib: MyAnnotationInLib: stringArrayParam = [3, 5, 7]
 // Str
 // 42
 // Foo
@@ -32,16 +35,12 @@
 // Array
 // @Foo
 // @Suppress
-// RGB.G
-// JavaEnum.ONE
+// G
+// ONE
 // 31
 // [warning1, warning 2]
 // Sub: [i:42]
 // TestJavaLib: OtherAnnotation
-// TestNestedAnnotationDefaults: def
-// TestNestedAnnotationDefaults: hij
-// TestNestedAnnotationDefaults: def
-// TestNestedAnnotationDefaults: hij
 // END
 // MODULE: module1
 // FILE: placeholder.kt
@@ -49,19 +48,13 @@
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.TYPE_USE})
-@interface MyAnnotation {
-    String stringParam() default "1";
+@interface MyAnnotationInLib {
+    String stringParam();
     String stringParam2() default "1";
     String[] stringArrayParam() default {"3", "5", "7"};
 }
-
-@interface Default {
-    Class<?>[] value();
-    int value1();
-}
-
 interface MyInterface {}
-@MyAnnotation(stringParam = "2") class MyClassInLib implements MyInterface {}
+@MyAnnotationInLib(stringParam = "2") class MyClassInLib implements MyInterface {}
 
 // FILE: OtherAnnotation.java
 import java.lang.annotation.Retention;
@@ -75,14 +68,17 @@ public @interface JavaAnnotationWithDefaults {
     OtherAnnotation otherAnnotationVal() default @OtherAnnotation("def");
 }
 
-// FILE: KotlinAnnotationWithDefaults.kt
-annotation class KotlinAnnotationWithDefaults(val otherAnnotation: OtherAnnotation = OtherAnnotation("hij"))
-
 // MODULE: main(module1)
 // FILE: Test.java
-@MyAnnotation(stringParam = "2") class MyClass implements MyInterface {}
-
+@Target({ElementType.TYPE, ElementType.TYPE_USE})
+@interface MyAnnotation {
+    String stringParam();
+    String stringParam2() default "1";
+    String[] stringArrayParam() default {"3", "5", "7"};
+}
+@MyAnnotation(stringParam = "2") @MyAnnotationInLib(stringParam = "2") class MyClass implements MyInterface {}
 // FILE: a.kt
+
 enum class RGB {
     R, G, B
 }
@@ -110,17 +106,16 @@ class C {
 
 }
 // FILE: JavaAnnotated.java
-@Default
 @Bar(argStr = "Str",
     argInt = 40 + 2,
     argClsUser = Foo.class,
-        argClsLib = java.io.File.class,
-argClsLocal = Local.class, // intentional error type
-argClsArray = kotlin.Array.class,
-argAnnoUser = @Foo(s = 17),
-argAnnoLib = @Suppress(names = {"name1", "name2"}),
-argEnum = RGB.G,
-argJavaNum = JavaEnum.ONE)
+    argClsLib = java.io.File.class,
+    argClsLocal = Local.class, // intentional error type
+    argClsArray = kotlin.Array.class,
+    argAnnoUser = @Foo(s = 17),
+    argAnnoLib = @Suppress(names = {"name1", "name2"}),
+    argEnum = RGB.G,
+    argJavaNum = JavaEnum.ONE)
 public class JavaAnnotated {}
 
 // FILE: JavaEnum.java
@@ -144,18 +139,3 @@ class Sub implements @B(a = @A(i = 42)) Parent {}
 // FILE: TestJavaLib.java
 @JavaAnnotationWithDefaults
 class TestJavaLib {}
-
-// FILE: JavaAnnotationWithDefaultsInSource.java
-public @interface JavaAnnotationWithDefaultsInSource {
-    OtherAnnotation otherAnnotationVal() default @OtherAnnotation("def");
-}
-
-// FILE: KotlinAnnotationWithDefaults.kt
-annotation class KotlinAnnotationWithDefaultsInSource(val otherAnnotation: OtherAnnotation = OtherAnnotation("hij"))
-
-// FILE: KotlinClient.kt
-@JavaAnnotationWithDefaultsInSource
-@KotlinAnnotationWithDefaultsInSource
-@JavaAnnotationWithDefaults
-@KotlinAnnotationWithDefaults
-class TestNestedAnnotationDefaults
