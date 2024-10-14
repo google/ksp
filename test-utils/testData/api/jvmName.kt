c11ac8d1dfe5fd1076d88541e4e5048b828bcf0d
Kuan-Ying Chou <kuanyingchou@google.com>
2024-10-14 17:23:58 +0100

    Test @JvmName on annotation property getters

--- test-utils/testData/api/jvmName.kt
+++ test-utils/testData/api/jvmName.kt
@@ -4,23 +4,32 @@
 // (getX, setX), (getY, null)
 // stringParameter
 // stringParameter
+// stringParameter
+// JvmName: stringParameter
+// JvmName: stringParameter
 // END
 // MODULE: lib
 // FILE: Lib.kt
 data class TestLibDataClass(var x: Int, val y: String)
+// FILE: MyAnnotationLib.kt
+annotation class MyAnnotationLib(
+    @get:JvmName("stringParameter")
+    val stringParam: String
+)
+// FILE: MyAnnotationUserLib.java
+@MyAnnotationLib(stringParameter = "foo")
+class MyAnnotationUserLib {}
+
+// MODULE: main(lib)
+// FILE: K.kt
 // FILE: MyAnnotation.kt
 annotation class MyAnnotation(
     @get:JvmName("stringParameter")
     val stringParam: String
 )
-// FILE: MyAnnotationUserLib.java
-@MyAnnotation(stringParameter = "foo")
-class MyAnnotationUserLib {}
-
-// MODULE: main(lib)
-// FILE: K.kt
 data class TestDataClass(var x: Int, val y: String)
 // FILE: MyAnnotationUser.java
+@MyAnnotationLib(stringParameter = "foo")
 @MyAnnotation(stringParameter = "foo")
 class MyAnnotationUser {}
 
