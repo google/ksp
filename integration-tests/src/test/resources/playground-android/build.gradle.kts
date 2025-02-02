ba5efd443dc448dbc9b48469417808b251a65172
Scott Pollom <spollom@google.com>
2025-02-01 16:17:04 -0800

    Make KSP compatible with Gradle's isolated projects
    WIP
    
    Bug: https://issuetracker.google.com/issues/361415637
    Test: AndroidIT.kt

--- integration-tests/src/test/resources/playground-android/build.gradle.kts
+++ integration-tests/src/test/resources/playground-android/build.gradle.kts
@@ -8,13 +8,3 @@ buildscript {
         google()
     }
 }
-
-allprojects {
-    val testRepo: String by project
-    repositories {
-        maven(testRepo)
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
-        google()
-    }
-}
