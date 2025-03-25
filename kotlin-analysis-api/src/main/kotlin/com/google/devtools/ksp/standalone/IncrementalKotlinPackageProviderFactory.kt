9068bda24fa459919646a7fe550e77a7f0e9d140
Ting-Yuan Huang <laszio@google.com>
2025-03-25 11:50:38 -0700

    Register KotlinStandalonePackageProviderFactory to project Disposable
    It has to be done manually because it is wrapped by
    IncrementalPackageProviderFactory.

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/standalone/IncrementalKotlinPackageProviderFactory.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/standalone/IncrementalKotlinPackageProviderFactory.kt
@@ -1,6 +1,8 @@
 package com.google.devtools.ksp.standalone
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.analysis.api.platform.packages.KotlinCompositePackageProvider
 import org.jetbrains.kotlin.analysis.api.platform.packages.KotlinPackageProvider
@@ -10,6 +12,7 @@ import org.jetbrains.kotlin.psi.KtFile
 
 class IncrementalKotlinPackageProviderFactory(
     private val project: Project,
+    private val projectDisposable: Disposable,
 ) : KotlinPackageProviderFactory {
     private val staticFactories: MutableList<KotlinStandalonePackageProviderFactory> = mutableListOf()
 
@@ -20,6 +23,7 @@ class IncrementalKotlinPackageProviderFactory(
 
     fun update(files: Collection<KtFile>) {
         val staticFactory = KotlinStandalonePackageProviderFactory(project, files)
+        Disposer.register(projectDisposable, staticFactory)
         staticFactories.add(staticFactory)
     }
 }
