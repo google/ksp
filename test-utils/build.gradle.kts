7087b28af6a2b4ed16cac8d99787d96c3090c020
Fedor Tolstonozhenko <fedor.tolstonozhenko@jetbrains.com>
2025-04-16 17:39:44 +0200

    Update url to JetBrains kotlin-ide-plugin-dependencies Maven Repositories

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 version = "2.0.255-SNAPSHOT"
 
 repositories {
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+    maven("https://redirector.kotlinlang.org/maven/kotlin-ide-plugin-dependencies")
     maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
 }
 
