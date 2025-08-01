2a8fd2ab3bb82b7f69b6a65953b3ff190cd7e9e3
Hakan Mehmed <hmehmed@google.com>
2025-08-01 15:09:56 +0000

    Add runtime warning when using KSP1

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/AndroidPluginIntegration.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/AndroidPluginIntegration.kt
@@ -20,6 +20,7 @@ import com.android.build.api.AndroidPluginVersion
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.api.SourceKind
+import com.google.devtools.ksp.gradle.utils.getAgpVersion
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.file.Directory
@@ -163,14 +164,7 @@ object AndroidPluginIntegration {
      * Returns true for older AGP versions or when AGP version cannot be determined.
      */
     fun Project.useLegacyVariantApi(): Boolean {
-        val agpVersion = try {
-            this.extensions
-                .findByType(com.android.build.api.variant.AndroidComponentsExtension::class.java)
-                ?.pluginVersion
-        } catch (e: Exception) {
-            // Perhaps a version of AGP before pluginVersion API was added.
-            null
-        }
+        val agpVersion = project.getAgpVersion()
 
         // Fall back to using the legacy Variant API if the AGP version can't be determined for now.
         if (agpVersion == null) {
