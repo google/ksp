c1e272b114a7c98efd88f75bc129f2786bd1b85d
Maya Muir <mkmuir@google.com>
2025-08-06 17:14:52 +0000

    Revert "UPDATE_KOTLIN_VERSION: 2.2.20-Beta2"
    This reverts commit 2124ef81af1b13e32b111da5bb5ecdb6366291a8.

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KotlinFactories.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KotlinFactories.kt
@@ -38,6 +38,7 @@ import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.process.CommandLineArgumentProvider
+import org.gradle.process.ExecOperations
 import org.gradle.work.Incremental
 import org.gradle.work.InputChanges
 import org.gradle.work.NormalizeLineEndings
@@ -170,7 +171,6 @@ class KotlinFactories {
                             it.name == konanTargetName
                         }
                     }
-                    kspTask.separateKmpCompilation.value(false)
                 }
             }
         }
@@ -307,11 +307,13 @@ abstract class KspTaskNative @Inject internal constructor(
     compilation: KotlinCompilationInfo,
     objectFactory: ObjectFactory,
     providerFactory: ProviderFactory,
+    execOperations: ExecOperations
 ) : KotlinNativeCompile(
         compilation,
         objectFactory.newInstance(KotlinNativeCompilerOptionsDefault::class.java),
         objectFactory,
         providerFactory,
+        execOperations
     ),
     KspTask
 
