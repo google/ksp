0fc3f15515f512251e1def70d383325081a83435
Hakan Mehmed <hmehmed@google.com>
2025-08-18 16:40:07 +0000

    Add integration test for AGP with built in kotlin
    (cherry picked from commit b39fb0a2e77928ceac39a0ba52131d2494b6add1)

---
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/AndroidBuiltInKotlinIT.kt
@@ -0,0 +1,36 @@
+package com.google.devtools.ksp.test
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+
+class AndroidBuiltInKotlinIT {
+    @Rule
+    @JvmField
+    val project: TemporaryTestProject = TemporaryTestProject("playground-android-builtinkotlin", "playground", true)
+
+    @Test
+    fun testPlaygroundAndroidWithBuiltInKotlin() {
+        val gradleRunner = GradleRunner.create().withProjectDir(project.root)
+
+        gradleRunner.withArguments(
+            "clean", "build", "minifyReleaseWithR8", "--configuration-cache", "--info", "--stacktrace"
+        ).build().let { result ->
+            Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":workload:build")?.outcome)
+            val mergedConfiguration = File(project.root, "workload/build/outputs/mapping/release/configuration.txt")
+            assert(mergedConfiguration.exists()) {
+                "Merged configuration file not found!\n${printDirectoryTree(project.root)}"
+            }
+            val configurationText = mergedConfiguration.readText()
+            assert("-keep class com.example.AClassBuilder { *; }" in configurationText) {
+                "Merged configuration did not contain generated proguard rules!\n$configurationText"
+            }
+            val outputs = result.output.lines()
+            assert("w: [ksp] [workload_debug] Mangled name for internalFun: internalFun\$workload_debug" in outputs)
+            assert("w: [ksp] [workload_release] Mangled name for internalFun: internalFun\$workload_release" in outputs)
+        }
+    }
+}
