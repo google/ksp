95d4cc2f71759e25883b29aee41d0c93b75a1d5f
Hakan Mehmed <hmehmed@google.com>
2025-08-18 16:40:07 +0000

    Add integration test for AGP with built in kotlin
    (cherry picked from commit b39fb0a2e77928ceac39a0ba52131d2494b6add1)

---
+++ integration-tests/src/test/resources/playground-android-builtinkotlin/workload/build.gradle.kts
@@ -0,0 +1,46 @@
+val testRepo: String by project
+
+plugins {
+    // DO NOT CHANGE THE ORDER.
+    id("com.google.devtools.ksp")
+    id("com.android.application")
+    id("com.android.experimental.built-in-kotlin")
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven(testRepo)
+    mavenCentral()
+    maven("https://redirector.kotlinlang.org/maven/bootstrap/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":test-processor"))
+    ksp(project(":test-processor"))
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            // For regression testing https://github.com/google/ksp/pull/467
+            proguardFiles.add(file("proguard-rules.pro"))
+            isMinifyEnabled = true
+        }
+    }
+}
+
+ksp {
+    arg("option1", "value1")
+    arg("option2", "value2")
+}
