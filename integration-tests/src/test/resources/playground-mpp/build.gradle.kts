403cb293921a91078fe04c984d6fca5a7e596b8e
Hakan Mehmed <hmehmed@google.com>
2025-11-07 11:56:33 +0000

    Add android target to MultiplatformIT test

--- integration-tests/src/test/resources/playground-mpp/build.gradle.kts
+++ integration-tests/src/test/resources/playground-mpp/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     kotlin("multiplatform") apply false
+    id("com.android.kotlin.multiplatform.library") apply false
 }
 
 val testRepo: String by project
 allprojects {
     repositories {
         maven(testRepo)
+        mavenCentral()
+        google()
         mavenCentral()
         maven("https://redirector.kotlinlang.org/maven/bootstrap/")
     }
