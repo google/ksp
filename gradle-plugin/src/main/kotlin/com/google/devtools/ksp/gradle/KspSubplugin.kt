24e6779dab15e6d6f2681e57682ad5459cf05423
Hakan Mehmed <hmehmed@google.com>
2025-11-25 13:29:41 +0000

    Move ksp task cache to a gradle build service
    (cherry picked from commit 45b5d32001f25edc93bcb23c525ed9a888b73f6d)

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/KspSubplugin.kt
@@ -203,7 +203,7 @@ class KspGradleSubplugin @Inject internal constructor(private val registry: Tool
             kotlinCompilation,
             kotlinCompileProvider,
             processorClasspath,
-            kspExtension
+            kspExtension,
         )
 
         val generatedSources = arrayOf(
