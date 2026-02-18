d4c868a64ea7e13e5bd5ce2ca98370f268875979
Jakob Villumsen <villumsen@google.com>
2026-02-18 16:38:47 +0100

    Add new example project

---
+++ examples/hello-world/processor/src/main/kotlin/HelloWorldProcessorProvider.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 Google LLC
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+/**
+ * Provides an instance of a [HelloWorldProcessor] (defined in this project).
+ * The KSP framework calls this class to construct our processor.
+ * KSP knows about this class because we defined it in the META-INF directory.
+ */
+class HelloWorldProcessorProvider : SymbolProcessorProvider {
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        return HelloWorldProcessor(environment.codeGenerator)
+    }
+}
