8054c795c9799f699f82edfc960175afa8db8cf7
Jakob Villumsen <villumsen@google.com>
2026-04-09 15:22:50 +0200

    Skip elements in CollectAnnotatedSymbolsPsiVisitor
    (cherry picked from commit 83e1b5d2e822afbb09e39207afefe22f6e184b37)

--- kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/visitor/CollectAnnotatedSymbolsPsiVisitor.kt
+++ kotlin-analysis-api/src/main/kotlin/com/google/devtools/ksp/impl/visitor/CollectAnnotatedSymbolsPsiVisitor.kt
@@ -5,15 +5,23 @@ import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiAnonymousClass
 import com.intellij.psi.PsiCodeBlock
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiExpression
+import com.intellij.psi.PsiImportList
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiModifierList
+import com.intellij.psi.PsiPackageStatement
 import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.psi.PsiTypeParameter
+import com.intellij.psi.javadoc.PsiDocComment
+import org.jetbrains.kotlin.kdoc.psi.api.KDoc
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtDeclarationModifierList
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtFileAnnotationList
 import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtImportList
+import org.jetbrains.kotlin.psi.KtObjectLiteralExpression
+import org.jetbrains.kotlin.psi.KtPackageDirective
 
 /**
  * A [PsiRecursiveElementWalkingVisitor] that collects [PsiElement]s which
@@ -36,10 +44,12 @@ class CollectAnnotatedSymbolsPsiVisitor : PsiRecursiveElementWalkingVisitor() {
                     result.add(parent.parent)
                     return
                 }
+
                 is KtFileAnnotationList -> {
                     result.add(parent.parent)
                     return
                 }
+
                 else ->
                     // Explicit crash
                     error("Unexpected Kotlin Psi element at ${parent.toLocation()}: ${parent.javaClass}")
@@ -54,10 +64,12 @@ class CollectAnnotatedSymbolsPsiVisitor : PsiRecursiveElementWalkingVisitor() {
                     result.add(parent)
                     return
                 }
+
                 is PsiModifierList -> {
                     result.add(parent.parent)
                     return
                 }
+
                 else ->
                     // Explicit crash
                     error("Unexpected Java Psi element at ${parent.toLocation()}: ${parent.javaClass}")
@@ -70,30 +82,111 @@ class CollectAnnotatedSymbolsPsiVisitor : PsiRecursiveElementWalkingVisitor() {
      * Returns `true` if the [PsiElement] is skippable.
      *
      * An element is skippable if one of the following holds:
-     * - It is a Kotlin function body
-     * - It is a Java method body
-     * - It is a Java anonymous class
+     * - It is a package directive
+     * - It is an import list
+     * - It is a doc comment
+     * - It is a Kotlin function or Java method body
+     * - It is a Kotlin object expression or Java anonymous class
      */
     private fun PsiElement.isSkippable(): Boolean =
-        // TODO: Skip elements we are not interested in visiting, e.g., imports.
-        isKotlinFunctionBody() ||
-            isJavaMethodBody() ||
-            isJavaAnonymousClass()
+        isPackageDirective() ||
+            isImport() ||
+            isDocComment() ||
+            isFunctionOrMethodBody() ||
+            isObjectOrAnonymousClass()
+
+    private fun PsiElement.isPackageDirective(): Boolean =
+        isKotlinPackageDirective() || isJavaPackageDirective()
+
+    /**
+     * Returns `true` if the [PsiElement] is either a Kotlin or Java import list.
+     */
+    private fun PsiElement.isImport(): Boolean =
+        isKotlinImportList() || isJavaImportList()
+
+    /**
+     * Return `true` if the [PsiElement] is either a Kotlin or Java doc-comment.
+     */
+    private fun PsiElement.isDocComment(): Boolean =
+        isKotlinDocComment() || isJavaDocComment()
+
+    /**
+     * Return `true` if the [PsiElement] is either a Kotlin function body or a Java method body.
+     */
+    private fun PsiElement.isFunctionOrMethodBody(): Boolean =
+        isKotlinFunctionBody() || isJavaMethodBody()
+
+    /**
+     * Return `true` if the [PsiElement] is either a Kotlin object expression or a Java anonymous class.
+     */
+    private fun PsiElement.isObjectOrAnonymousClass(): Boolean =
+        isKotlinObjectExpression() || isJavaAnonymousClass()
+
+    /**
+     * Returns `true` is the [PsiElement] is a Kotlin package directive, e.g., `package com.example`
+     */
+    private fun PsiElement.isKotlinPackageDirective(): Boolean =
+        this is KtPackageDirective
+
+    /**
+     * Returns `true` is the [PsiElement] is a Java package directive, e.g., `package com.example`
+     */
+    private fun PsiElement.isJavaPackageDirective(): Boolean =
+        this is PsiPackageStatement
+
+    /**
+     * Returns `true` if the [PsiElement] is a Kotlin import list.
+     */
+    private fun PsiElement.isKotlinImportList(): Boolean =
+        this is KtImportList
+
+    /**
+     * Returns `true` if the [PsiElement] is a Java import list.
+     */
+    private fun PsiElement.isJavaImportList(): Boolean =
+        this is PsiImportList
+
+    /**
+     * Returns `true` if the [PsiElement] is a Kotlin doc comment.
+     */
+    private fun PsiElement.isKotlinDocComment(): Boolean =
+        this is KDoc
+
+    /**
+     * Returns `true` if the [PsiElement] is a Java doc-comment.
+     */
+    private fun PsiElement.isJavaDocComment(): Boolean =
+        this is PsiDocComment
 
     /**
      * Returns `true` if the [PsiElement] is a Kotlin function body.
      */
-    private fun PsiElement.isKotlinFunctionBody(): Boolean =
-        this is KtExpression &&
-            this.parent is KtFunction &&
-            (this.parent as KtFunction).bodyExpression == this
+    private fun PsiElement.isKotlinFunctionBody(): Boolean = when (this) {
+        is KtExpression -> when (val parent = this.parent) {
+            is KtFunction ->
+                parent.bodyExpression == this || parent.bodyBlockExpression == this
+
+            else -> false
+        }
+
+        else -> false
+    }
 
     /**
      * Returns `true` if the [PsiElement] is a Java method body.
      */
     private fun PsiElement.isJavaMethodBody(): Boolean =
-        this is PsiCodeBlock &&
-            this.parent is PsiMethod
+        this is PsiExpression ||
+            (
+                this is PsiCodeBlock &&
+                    this.parent is PsiMethod
+                )
+
+    /**
+     * Returns `true` if the [PsiElement] is a Kotlin object expression.
+     */
+    private fun PsiElement.isKotlinObjectExpression(): Boolean =
+        this is KtObjectLiteralExpression
 
     /**
      * Returns `true` if the [PsiElement] is a Java anonymous class.
