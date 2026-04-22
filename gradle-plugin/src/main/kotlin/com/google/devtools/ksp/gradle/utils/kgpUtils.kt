677a9b816385394c0a771363ce5bf6937183e93d
Hakan Mehmed <hmehmed@google.com>
2026-04-22 09:23:27 +0000

    Enable project ksp project isolation when org.gradle.unsafe.isolated-projects=true
    Fixes #2866
    
    (cherry picked from commit 4367bd954fbd07f540f1aff93bb9c2e568023743)

--- gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/utils/kgpUtils.kt
+++ gradle-plugin/src/main/kotlin/com/google/devtools/ksp/gradle/utils/kgpUtils.kt
@@ -25,5 +25,7 @@ fun Project.canUseGeneratedKotlinApi(): Boolean {
 }
 
 fun Project.enableProjectIsolationCompatibleCodepath(): Boolean {
-    return providers.gradleProperty("ksp.project.isolation.enabled").orNull == "true"
+    return providers.gradleProperty("ksp.project.isolation.enabled").orNull == "true" ||
+        providers.gradleProperty("org.gradle.unsafe.isolated-projects").orNull == "true" ||
+        providers.systemProperty("org.gradle.unsafe.isolated-projects").orNull == "true"
 }
