fdf21b5df68fc19afa43e9f75ddec492c9882cfd
Jakob Villumsen <villumsen@google.com>
2026-05-06 12:57:06 +0200

    Add Negative test
    (cherry picked from commit c693bb95f142b8b410d2d9b311eb7cf20a376ce1)

---
+++ kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/test/Negative.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2026 Google LLC
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.ksp.test
+
+/**
+ * Indicates that a test case is a "negative test," validating how KSP handles invalid or problematic input.
+ *
+ * A negative test typically verifies that KSP correctly identifies errors, reports them appropriately,
+ * or handles invalid code constructs without crashing.
+ *
+ * @property description An optional brief explanation of the invalid scenario or expected failure.
+ */
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS)
+annotation class Negative(val description: String = "")
