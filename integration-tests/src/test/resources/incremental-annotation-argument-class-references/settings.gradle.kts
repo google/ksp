b5d7b41a8a0aabec20003fe7435a80055270c6c8
Jakob Villumsen <villumsen@google.com>
2026-07-01 14:17:13 +0200

    Add negative test for incremental compilation
    This test shows the presence of the bug where KSP does not capture transitive dependencies of class references, e.g., `MyClass::class`.

---
+++ integration-tests/src/test/resources/incremental-annotation-argument-class-references/settings.gradle.kts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2026 Google LLC
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pluginManagement {
+    val kspVersion: String by settings
+    val kotlinVersion: String by settings
+    val testRepo: String by settings
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
+        kotlin("jvm") version kotlinVersion
+    }
+    repositories {
+        maven(testRepo)
+        gradlePluginPortal()
+        maven("https://redirector.kotlinlang.org/maven/bootstrap/")
+    }
+}
+
+rootProject.name = "incremental-test-annotation-argument-class-references"
+
+include(":downstream")
+include(":upstream")
+include(":processor")
