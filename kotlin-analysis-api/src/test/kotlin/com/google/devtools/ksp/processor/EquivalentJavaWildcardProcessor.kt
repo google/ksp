a7239468ffc41669d81caa5c1f8ddf2aefd84da5
Jakob Villumsen <villumsen@google.com>
2026-07-27 13:37:55 +0200

    Move EquivalentJavaWildcardProcessor to `kotlin-analysis-api`
    (cherry picked from commit 273a2db69b53bca5c49e16ac9e37cbfef56f1383)

--- kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/processor/EquivalentJavaWildcardProcessor.kt
+++ kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/processor/EquivalentJavaWildcardProcessor.kt
@@ -19,7 +19,9 @@ package com.google.devtools.ksp.processor
 
 import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.Resolver
-import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.KSTypeReference
 import com.google.devtools.ksp.visitor.KSTopDownVisitor
 
 open class EquivalentJavaWildcardProcessor : AbstractTestProcessor() {
