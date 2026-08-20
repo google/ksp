8b3028d2467db48f80fb6864d991e93e3b2562dd
Jakob Villumsen <villumsen@google.com>
2026-08-20 14:45:08 +0200

    Increase metaspace in integration test projects to 2048m
    At least one integration test failed due to OOM errors in the meta space.

--- integration-tests/src/test/kotlin/com/google/devtools/ksp/test/fixtures/TemporaryTestProject.kt
+++ integration-tests/src/test/kotlin/com/google/devtools/ksp/test/fixtures/TemporaryTestProject.kt
@@ -43,8 +43,8 @@ class TemporaryTestProject(
             appendProperty("org.gradle.parallel=false")
         }
 
-        appendProperty("org.gradle.jvmargs=-Xmx1024m -XX:MaxMetaspaceSize=1024m")
-        appendProperty("kotlin.daemon.jvmargs=-Xmx1024m -XX:MaxMetaspaceSize=1024m")
+        appendProperty("org.gradle.jvmargs=-Xmx1024m -XX:MaxMetaspaceSize=2048m")
+        appendProperty("kotlin.daemon.jvmargs=-Xmx1024m -XX:MaxMetaspaceSize=2048m")
 
         // To debug compiler and compiler plugin:
         // 1. s/kotlin-compiler/kotlin-compiler-embeddable in integration-tests/build.gradle.kts, and
