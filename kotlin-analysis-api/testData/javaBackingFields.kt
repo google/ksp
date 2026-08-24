4dcf5a41f6e705259ef6a14ba9f671f4999c6174
Jakob Villumsen <villumsen@google.com>
2026-08-24 16:00:14 +0200

    Update javaBackingFields test

--- kotlin-analysis-api/testData/javaBackingFields.kt
+++ kotlin-analysis-api/testData/javaBackingFields.kt
@@ -19,32 +19,32 @@
 // EXPECTED:
 // lib.Fields
 // - lib.Fields.x: kotlin.Int - [PRIVATE, FINAL]
-// - lib.Fields.x.field: kotlin.Int - [PRIVATE, FINAL]
+// EXPECT NEXT: - lib.Fields.x.field: kotlin.Int - [PRIVATE, FINAL]
 // - lib.Fields.y: kotlin.Int - [PUBLIC, FINAL]
-// - lib.Fields.y.field: kotlin.Int - [PUBLIC, FINAL]
+// EXPECT NEXT: - lib.Fields.y.field: kotlin.Int - [PUBLIC, FINAL]
 // - lib.Fields.z: kotlin.Int - [PROTECTED, FINAL]
-// - lib.Fields.z.field: kotlin.Int - [PROTECTED, FINAL]
+// EXPECT NEXT: - lib.Fields.z.field: kotlin.Int - [PROTECTED, FINAL]
 // - lib.Fields.w: kotlin.Int - [FINAL]
-// - lib.Fields.w.field: kotlin.Int - [FINAL]
+// EXPECT NEXT: - lib.Fields.w.field: kotlin.Int - [FINAL]
 // lib.AccessorsOnly
 // lib.ObscureFields
 // - lib.ObscureFields.back: kotlin.Int - [PRIVATE, FINAL]
-// - lib.ObscureFields.back.field: kotlin.Int - [PRIVATE, FINAL]
+// EXPECT NEXT: - lib.ObscureFields.back.field: kotlin.Int - [PRIVATE, FINAL]
 // SourceFields
 // - SourceFields.x: kotlin.Int - [PRIVATE]
-// - SourceFields.x.field: kotlin.Int - [PRIVATE]
+// EXPECT NEXT: - SourceFields.x.field: kotlin.Int - [PRIVATE]
 // - SourceFields.y: kotlin.Int - [PUBLIC]
-// - SourceFields.y.field: kotlin.Int - [PUBLIC]
+// EXPECT NEXT: - SourceFields.y.field: kotlin.Int - [PUBLIC]
 // - SourceFields.z: kotlin.Int - [PROTECTED]
-// - SourceFields.z.field: kotlin.Int - [PROTECTED]
+// EXPECT NEXT: - SourceFields.z.field: kotlin.Int - [PROTECTED]
 // - SourceFields.w: kotlin.Int - []
-// - SourceFields.w.field: kotlin.Int - []
+// EXPECT NEXT: - SourceFields.w.field: kotlin.Int - []
 // SourceAccessorsOnly
 // SourceObscureFields
 // - SourceObscureFields.back: kotlin.Int - [PRIVATE]
-// - SourceObscureFields.back.field: kotlin.Int - [PRIVATE]
+// EXPECT NEXT: - SourceObscureFields.back.field: kotlin.Int - [PRIVATE]
 // - SourceObscureFields.unused: kotlin.Boolean - [PRIVATE, FINAL]
-// - SourceObscureFields.unused.field: kotlin.Boolean - [PRIVATE, FINAL]
+// EXPECT NEXT: - SourceObscureFields.unused.field: kotlin.Boolean - [PRIVATE, FINAL]
 // END
 
 // MODULE: lib
