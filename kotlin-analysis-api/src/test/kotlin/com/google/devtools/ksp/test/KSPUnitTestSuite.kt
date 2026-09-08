a5e9e7a51268bf3b976cdc9dcf0f44d0d1afbef5
Jakob Villumsen <villumsen@google.com>
2026-09-08 13:57:55 +0200

    Mark testNativeAnnotationDefaultValue as fixed

--- kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/test/KSPUnitTestSuite.kt
+++ kotlin-analysis-api/src/test/kotlin/com/google/devtools/ksp/test/KSPUnitTestSuite.kt
@@ -1039,11 +1039,11 @@ abstract class KSPUnitTestSuite(
         runTest("$AA_PATH/native/packageDeclarationsMultiRound.kt")
     }
 
-    @Bug("https://github.com/google/ksp/issues/2356", BugState.OPEN)
+    @Bug("https://github.com/google/ksp/issues/2356", BugState.FIXED)
     @TestMetadata("native/annotationDefaultValue.kt")
     @Test
     fun testNativeAnnotationDefaultValue() {
-        runThrowingTest("$AA_PATH/native/annotationDefaultValue.kt", java.util.NoSuchElementException::class)
+        runTest("$AA_PATH/native/annotationDefaultValue.kt")
     }
 
     @Bug(
