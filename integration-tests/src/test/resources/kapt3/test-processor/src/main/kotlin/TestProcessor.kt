cc194da7f5f57e98ecd09818ad0464223fc7d0a0
Jakob Villumsen <villumsen@google.com>
2026-09-15 11:59:12 +0200

    Update integration tests with visitContextParameter
    (cherry picked from commit 9acf923052a947130f60de89781cb04e59d75e16)

--- integration-tests/src/test/resources/kapt3/test-processor/src/main/kotlin/TestProcessor.kt
+++ integration-tests/src/test/resources/kapt3/test-processor/src/main/kotlin/TestProcessor.kt
@@ -227,6 +227,9 @@ class TestProcessor : SymbolProcessor {
             // Empty impl for now
         }
 
+        override fun visitContextParameter(contextParameter: KSContextParameter, data: String) {
+            // Empty impl for now
+        }
         override fun visitTypeReference(typeReference: KSTypeReference, data: String) {
             if (checkVisited(typeReference)) return
             typeReference.annotations.forEach { it.accept(this, "$data  ") }
